f2c7ea3dd293e50c6330b2433d814a07fa0d2c38
hazman1yusof <hazman.yusof@gmail.com>
2018-01-24 16:13:51 +0800

    betulkan login

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Company;
+use App\model\sysdb\Company;
 use App\User;
 use Auth;
 use Hash;
--- app/model/sysdb/sex.php
+++ app/model/sysdb/sex.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\model\sysdb;
 
 use Illuminate\Database\Eloquent\Model;
 use App\defaultModel;
