8ed7ced39b6d740577b85b2d76d4262f64af5230
hazman1yusof <hazman.yusof@gmail.com>
2018-01-24 16:30:48 +0800

    adasd

--- resources/views/hisdb/appointment/doctor_maintenance.blade.php
+++ resources/views/hisdb/appointment/doctor_maintenance.blade.php
@@ -3,12 +3,37 @@
 @section('title', 'Doctor Maintenance Setup')
 
 @section('body')
+	<div class='row'>
+		<form id="searchForm" class="formclass" style='width:99%'>
+			<fieldset>
+				<div class="ScolClass" style="padding:0 0 0 15px">
+					<div name='Scol'>Search By : </div>
+				</div>
+				<div class="StextClass">
+					<input name="Stext" type="search" placeholder="Search here ..." class="form-control text-uppercase">
+				</div>
+			 </fieldset> 
+		</form>
+		
+		  <div class="panel panel-default">
+			<div class="panel-body">
+				<div class='col-md-12' style="padding:0 0 15px 0">
+					<table id="jqGrid" class="table table-striped"></table>
+						<div id="jqGridPager"></div>
+				</div>
+			</div>
+		</div>
 
-     <a class='pull-right pointer text-primary' id='PHBtn'><span class='fa fa-calendar fa-lg '></span> Public Holiday </a>
-    <a class='pull-right pointer text-primary' id='ALBtn'><span class='fa fa-calendar fa-lg '></span> Leave &nbsp;&nbsp;</a>
-    <a class='pull-right pointer text-primary' id='TSBtn'><span class='fa fa-clock-o fa-lg '></span> Time Session&nbsp;&nbsp; </a>
-		<br><br><br>
-	@include('layouts.default_search_and_table')
+		<a class='pull-right pointer text-primary' id='PHBtn'>
+	     	<span class='fa fa-calendar fa-lg '></span> Public Holiday 
+	    </a>
+	    <a class='pull-right pointer text-primary' id='ALBtn'>
+	    	<span class='fa fa-calendar fa-lg '></span> Leave &nbsp;&nbsp;
+	    </a>
+	    <a class='pull-right pointer text-primary' id='TSBtn'>
+	    	<span class='fa fa-clock-o fa-lg '></span> Time Session&nbsp;&nbsp; 
+	    </a>
+	</div>
     
 	 <div id="TSBox" title="Time Session" style="display:none">
     <ul>
