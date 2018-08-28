579c9f1d2284020e6918200aaf06c978e796fa4e
hazman1yusof <hazman.yusof@gmail.com>
2018-08-28 10:39:03 +0800

    asdsad

--- app/Http/Controllers/defaultController.php
+++ app/Http/Controllers/defaultController.php
@@ -85,10 +85,9 @@ abstract class defaultController extends Controller{
         //////////join//////////
         if(!empty($request->join_onCol)){
             foreach ($request->join_onCol as $key => $value) {
-
                 if(empty($request->join_filterCol)){ //ni nak check kalu ada AND lepas JOIN ON
 
-                     if($request->join_type[$key] == 'LEFT JOIN'){
+                    if($request->join_type[$key] == 'LEFT JOIN'){
 
                         $table = $table->leftJoin($request->table_name[$key+1], $request->join_onCol[$key], '=', $request->join_onVal[$key]);
 
@@ -99,35 +98,40 @@ abstract class defaultController extends Controller{
 
                 }else{
 
-                    if($request->join_type[$key] == 'LEFT JOIN'){
+                   if($request->join_type[$key] == 'LEFT JOIN'){
 
                         $table = $table->leftJoin($request->table_name[$key+1], function($join) use ($request,$key){
                             $join = $join->on($request->join_onCol[$key], '=', $request->join_onVal[$key]);
                             
-                            foreach ($request->join_filterCol as $key2 => $value2) {
-                                foreach ($value2 as $key3 => $value3) {
-                                    $pieces = explode(' ', $value3);
+                            if(isset($request->join_filterCol[$key])){
+                                foreach ($request->join_filterCol[$key] as $key2 => $value2) {
+                                    $pieces = explode(' ', $value2);
                                     if($pieces[1] == 'on'){
-                                        $join = $join->on($pieces[0],$pieces[2],$request->join_filterVal[$key2][$key3]);
+                                        $join = $join->on($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
                                     }else{
-                                        $join = $join->where($pieces[0],$pieces[2],$request->join_filterVal[$key2][$key3]);
+                                        $join = $join->where($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
                                     }
                                 }
                             }
+                            
                         });
 
                     }else{
 
                         $table = $table->join($request->table_name[$key+1], function($join) use ($request,$key){
                             $join = $join->on($request->join_onCol[$key], '=', $request->join_onVal[$key]);
-                            foreach ($request->join_filterCol[$key] as $key2 => $value2) {
-                                $pieces = explode(' ', $value2);
-                                if($pieces[1] == 'on'){
-                                    $join = $join->on($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
-                                }else{
-                                    $join = $join->where($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
+
+                            if(isset($request->join_filterCol[$key])){
+                                foreach ($request->join_filterCol[$key] as $key2 => $value2) {
+                                    $pieces = explode(' ', $value2);
+                                    if($pieces[1] == 'on'){
+                                        $join = $join->on($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
+                                    }else{
+                                        $join = $join->where($pieces[0],$pieces[2],$request->join_filterVal[$key][$key2]);
+                                    }
                                 }
                             }
+                            
                         });
 
                     }
