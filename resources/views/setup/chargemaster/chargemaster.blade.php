7b05c992197078918b48e8499e5c52e000a6babb
AmiraAlias <nuramiralias@gmail.com>
2019-06-18 11:56:42 -0700

    do

--- resources/views/setup/chargemaster/chargemaster.blade.php
+++ resources/views/setup/chargemaster/chargemaster.blade.php
@@ -88,81 +88,86 @@
 	    		</div>'
 		    </div>
 		</div> 
+
     </div>
 	<!-- ***************End Search + table ********************* -->
 
 	<div id="dialogForm" title="Add Form" >
-		<form class='form-horizontal' style='width:99%' id='formdata'>
-
-			{{ csrf_field() }}
-			<input type="hidden" name="idno">
-
-				<div class="form-group">
-				  	<label class="col-md-2 control-label" for="areacode">Charge Code</label>  
-                    <div class="col-md-3">
-                    	<input id="chgcode" name="chgcode" type="text" class="form-control input-sm" data-validation="required" frozeOnEdit>
-                    </div>
-				
-                	<label class="col-md-2 control-label" for="description">Group</label>  
-                    <div class="col-md-3">
-                    	<input id="chggroup" name="chggroup" type="text" class="form-control input-sm" data-validation="number, required">
-                    </div>
-				</div>
-                
-                <div class="form-group">
-                	<label class="col-md-2 control-label" for="description">Description</label>  
-                	<div class="col-md-6">
-                		<input id="description" name="description" type="text" class="form-control input-sm" data-validation="required">
-                	</div>
-				</div>    
-                
-				<div class="form-group">
-					<label class="col-md-2 control-label" for="adduser">Created By</label>  
-					<div class="col-md-3">
-						<input id="adduser" name="adduser" type="text" class="form-control input-sm" frozeOnEdit hideOne>
-					</div>
-
-					<label class="col-md-2 control-label" for="upduser">Last Entered</label>  
-					<div class="col-md-3">
-						<input id="upduser" name="upduser" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
-					</div>
-				</div> 
-
-				<div class="form-group">
-					<label class="col-md-2 control-label" for="adddate">Created Date</label>  
-					<div class="col-md-3">
-						<input id="adddate" name="adddate" type="text" class="form-control input-sm" frozeOnEdit hideOne>
-					</div>
-
-					<label class="col-md-2 control-label" for="upddate">Last Entered Date</label>  
-					<div class="col-md-3">
-						<input id="upddate" name="upddate" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
-					</div>
-				</div>  
-
-				<div class="form-group">
-					<label class="col-md-2 control-label" for="lastcomputerid">Computer Id</label>  
-					<div class="col-md-3">
-						<input id="lastcomputerid" name="lastcomputerid" type="text" class="form-control input-sm" data-validation="required" rdonly >
-					</div>
-
-					<label class="col-md-2 control-label" for="lastipaddress">IP Address</label>  
-					<div class="col-md-3">
-						<input id="lastipaddress" name="lastipaddress" type="text" maxlength="30" class="form-control input-sm" data-validation="required" rdonly >
-					</div>
-				</div>  
-
-		</form>
-	</div>
-
 		<div class='panel panel-info'>
-				<div class="panel-heading">Charge Price Detail</div>
+			<div class="panel-heading">Charge Master
+					</div>
+				<div class="panel-body" style="position: relative;">
+					<form class='form-horizontal' style='width:99%' id='formdata'>
+							{{ csrf_field() }}
+							<input id="idno" name="idno" type="hidden">
+						
+
+							<div class="form-group">
+							  	<label class="col-md-2 control-label" for="areacode">Charge Code</label>  
+			                    <div class="col-md-3">
+			                    	<input id="chgcode" name="chgcode" type="text" class="form-control input-sm" data-validation="required" frozeOnEdit>
+			                    </div>
+							
+			                	<label class="col-md-2 control-label" for="description">Group</label>  
+			                    <div class="col-md-3">
+			                    	<input id="chggroup" name="chggroup" type="text" class="form-control input-sm" data-validation="number, required">
+			                    </div>
+							</div>
+			                
+			                <div class="form-group">
+			                	<label class="col-md-2 control-label" for="description">Description</label>  
+			                	<div class="col-md-6">
+			                		<input id="description" name="description" type="text" class="form-control input-sm" data-validation="required">
+			                	</div>
+							</div>    
+			                
+							<div class="form-group">
+								<label class="col-md-2 control-label" for="adduser">Created By</label>  
+								<div class="col-md-3">
+									<input id="adduser" name="adduser" type="text" class="form-control input-sm" frozeOnEdit hideOne>
+								</div>
+
+								<label class="col-md-2 control-label" for="upduser">Last Entered</label>  
+								<div class="col-md-3">
+									<input id="upduser" name="upduser" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
+								</div>
+							</div> 
+
+							<div class="form-group">
+								<label class="col-md-2 control-label" for="adddate">Created Date</label>  
+								<div class="col-md-3">
+									<input id="adddate" name="adddate" type="text" class="form-control input-sm" frozeOnEdit hideOne>
+								</div>
+
+								<label class="col-md-2 control-label" for="upddate">Last Entered Date</label>  
+								<div class="col-md-3">
+									<input id="upddate" name="upddate" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
+								</div>
+							</div>  
+
+							<div class="form-group">
+								<label class="col-md-2 control-label" for="lastcomputerid">Computer Id</label>  
+								<div class="col-md-3">
+									<input id="lastcomputerid" name="lastcomputerid" type="text" class="form-control input-sm" data-validation="required" rdonly >
+								</div>
+
+								<label class="col-md-2 control-label" for="lastipaddress">IP Address</label>  
+								<div class="col-md-3">
+									<input id="lastipaddress" name="lastipaddress" type="text" maxlength="30" class="form-control input-sm" data-validation="required" rdonly >
+								</div>
+							</div>  
+					</form>
+				</div>
+			</div>
+			
+			<div class='panel panel-info'>
+				<div class="panel-heading">Inventory Transaction Detail</div>
 					<div class="panel-body">
 						<form id='formdata2' class='form-vertical' style='width:99%'>
-							<!-- <input id="gstpercent" name="gstpercent" type="hidden">
-							<input id="convfactor_uom" name="convfactor_uom" type="hidden" value='1'>
-							<input id="convfactor_pouom" name="convfactor_pouom" type="hidden" value='1'> -->
-							<input type="hidden" id="jqgrid2_itemcode_refresh" name="" value="0">
+							<input id="gstpercent" name="gstpercent" type="hidden">
+							<input id="convfactoruomcodetrdept" name="convfactoruomcodetrdept" type="hidden" value='1'>
+							<input id="convfactoruomcoderecv" name="convfactoruomcoderecv" type="hidden" value='1'>
+
 							<div id="jqGrid2_c" class='col-md-12'>
 								<table id="jqGrid2" class="table table-striped"></table>
 					            <div id="jqGridPager2"></div>
@@ -171,10 +176,12 @@
 					</div>
 
 					<div class="panel-body">
-						<div class="noti"><ol></ol>
+						<div class="noti" style="font-size: bold; color: red"><ol></ol>
 						</div>
 					</div>
-			</div>
+			</div>		
+			
+		</div>
 
 @endsection
 
