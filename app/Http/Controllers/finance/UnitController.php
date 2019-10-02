98828aa94c8048feb5512507203956d2c494805c
AmiraAlias <nuramiralias@gmail.com>
2019-10-02 09:47:28 -0700

    unit duplicatecheck

--- app/Http/Controllers/finance/UnitController.php
+++ app/Http/Controllers/finance/UnitController.php
@@ -4,19 +4,23 @@ namespace App\Http\Controllers\finance;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\defaultController;
-use DB;
-
+/*use DB;
+*/
 class UnitController extends defaultController
 {   
+    var $table;
+    var $duplicateCode;
 
     public function __construct()
     {
         $this->middleware('auth');
+        $this->duplicateCode = "sectorcode";
     }
 
     public function show(Request $request)
     {   
         return view('finance.GL.unit.unit');
+        
     }
 
     public function form(Request $request)
