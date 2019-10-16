f2eea81797d36a81a99ba6b1a7898428949f62b7
AmiraAlias <nuramiralias@gmail.com>
2019-10-16 15:09:59 -0700

    num 2words

--- public/plugins/jQuerynum2words/jquery.num2words.js
+++ public/plugins/jQuerynum2words/jquery.num2words.js
@@ -1,33 +1,4 @@
-/*
-Num2Words! jQuery Plugin (https://github.com/faizalmansor/num2words)
-@author Faizal Mansor (osh@okijana.com) [http://okijana.com]
-[Original base code & idea by Abhishek Sanoujam [http://abhisanoujam.blogspot.com/] (thanks!)]
 
-Num2Words v0.1
-Release Date: November 23, 2011
-
-MIT License
-Copyright (c) 2011 Faizal Mansor
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 (function($){
    $.fn.extend({ 
       num2words: function(options) {
@@ -136,7 +107,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 							};
 							i++;
 						};
-						if (word!='') word = word.toUpperCase() + ' DOLLARS';
+						if (word!='') word = word.toUpperCase() + ' RINGGIT';
 						if (word2!='') word2 = ' AND ' + word2.toUpperCase() + ' CENTS';
 						return word+word2;
 
