bc70113f99fac5d032b52aa43a85844def240318
AmiraAlias <nuramiralias@gmail.com>
2020-01-06 09:44:55 +0800

    unit

--- app/Http/Controllers/finance/UnitController.php
+++ app/Http/Controllers/finance/UnitController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\finance;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\defaultController;
+use stdClass;
+use DB;
+use DateTime;
+use Carbon\Carbon;
 
 class UnitController extends defaultController
 {   
@@ -26,13 +30,84 @@ class UnitController extends defaultController
     {   
         switch($request->oper){
             case 'add':
-                return $this->defaultAdd($request);
+                return $this->add($request);
             case 'edit':
-                return $this->defaultEdit($request);
+                return $this->edit($request);
             case 'del':
-                return $this->defaultDel($request);
+                return $this->del($request);
             default:
                 return 'error happen..';
         }
+    }
+
+    public function add(Request $request){
+
+        DB::beginTransaction();
+        try {
+
+            $sector = DB::table('sysdb.sector')
+                            ->where('sectorcode','=',$request->sectorcode);
+
+            if($sector->exists()){
+                throw new \Exception("record duplicate");
+            }
+
+            DB::table('sysdb.sector')
+                ->insert([  
+                    'compcode' => session('compcode'),
+                    'sectorcode' => strtoupper($request->sectorcode),
+                    'description' => strtoupper($request->description),
+                    'regioncode' => strtoupper($request->regioncode),
+                    'recstatus' => strtoupper($request->recstatus),
+                    //'idno' => strtoupper($request->idno),
+                    'lastcomputerid' => strtoupper($request->lastcomputerid),
+                    'lastipaddress' => strtoupper($request->lastipaddress),
+                    'adduser' => session('username'),
+                    'adddate' => Carbon::now("Asia/Kuala_Lumpur")
+                ]);
+
+             DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return response('Error'.$e, 500);
+        }
+    }
+
+    public function edit(Request $request){
+        
+        DB::beginTransaction();
+        try {
+
+            DB::table('sysdb.sector')
+                ->where('idno','=',$request->idno)
+                ->update([  
+                    'sectorcode' => strtoupper($request->sectorcode),
+                    'description' => strtoupper($request->description),
+                    'regioncode' => strtoupper($request->regioncode),
+                    'recstatus' => strtoupper($request->recstatus),
+                    'idno' => strtoupper($request->idno),
+                    'lastcomputerid' => strtoupper($request->lastcomputerid),
+                    'lastipaddress' => strtoupper($request->lastipaddress),
+                    'upduser' => session('username'),
+                    'upddate' => Carbon::now("Asia/Kuala_Lumpur")
+                ]); 
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return response('Error'.$e, 500);
+        }
+    }
+
+    public function del(Request $request){
+        DB::table('sysdb.sector')
+            ->where('idno','=',$request->idno)
+            ->update([  
+                'recstatus' => 'D',
+                'deluser' => session('username'),
+                'deldate' => Carbon::now("Asia/Kuala_Lumpur")
+            ]);
     }
 }
--- public/js/finance/GL/unit/unit.js
+++ public/js/finance/GL/unit/unit.js
@@ -47,14 +47,18 @@ $(document).ready(function () {
 			{ label: 'idno', name: 'idno', width: 10, hidden: true, key:true},
 			{ label: 'Unit', name: 'sectorcode', width: 20, classes: 'wrap', canSearch: true, editable: true, editrules: { required: true }, editoptions: {style: "text-transform: uppercase"}},
 			{ label: 'Description', name: 'description', width: 80, classes: 'wrap', canSearch: true, checked:true, editable: true, editrules: { required: true }, editoptions: {style: "text-transform: uppercase"}},
-			{ label: 'Section', name: 'regioncode', width: 50, classes: 'wrap'},
+			{ label: 'Section', name: 'regioncode', width: 50, classes: 'wrap', editable:true,
+					editrules:{required: true,custom:true, custom_func:cust_rules},formatter: showdetail,
+						edittype:'custom',	editoptions:
+						    {  custom_element:unitCustomEdit,
+						       custom_value:galGridCustomValue 	
+						    },
+			},
 			{ label: 'adduser', name: 'adduser', width: 90, hidden: true, classes: 'wrap' },
 			{ label: 'adddate', name: 'adddate', width: 90, hidden: true, classes: 'wrap' },
 			{ label: 'upduser', name: 'upduser', width: 90, hidden: true, classes: 'wrap' },
 			{ label: 'upddate', name: 'upddate', width: 90, hidden: true, classes: 'wrap' },
-			{ label: 'Record Status', name: 'recstatus', width: 20, classes: 'wrap', formatter:formatterstatus, unformat:unformatstatus, cellattr: function(rowid, cellvalue)
-							{return cellvalue == 'Deactive' ? 'class="alert alert-danger"': ''}, 
-			},
+			{ label: 'Record Status', name: 'recstatus', width: 20, classes: 'wrap', hidden: false, editable: true, edittype:"select",formatter:'select', editoptions:{value:"A:ACTIVE;D:DEACTIVE"}},
 			{ label: 'computerid', name: 'computerid', width: 90, hidden:true},
 			{ label: 'ipaddress', name: 'ipaddress', width: 90, hidden:true},
 			{ label: 'lastcomputerid', name: 'lastcomputerid', width: 90, hidden:true},
@@ -70,73 +74,219 @@ $(document).ready(function () {
 		height: 350,
 		rowNum: 30,
 		pager: "#jqGridPager",
-		ondblClickRow: function (rowid, iRow, iCol, e) {
-			$("#jqGridPager td[title='Edit Selected Row']").click();
-		},
-		gridComplete: function () {
-			if (oper == 'add') {
-				$("#jqGrid").setSelection($("#jqGrid").getDataIDs()[0]);
-			}
+		loadComplete: function(){
+			if(addmore_jqgrid.more == true){$('#jqGrid2_iladd').click();}
+				else{
+						$('#jqGrid2').jqGrid ('setSelection', "1");
+					}
 
-			$('#' + $("#jqGrid").jqGrid('getGridParam', 'selrow')).focus();
+				addmore_jqgrid.edit = addmore_jqgrid.more = false; //reset
+			},
+		ondblClickRow: function(rowid, iRow, iCol, e){
+			$("#jqGrid_iledit").click();
 		},
 
 	});
 
 	
-	/////////////////////////start grid pager/////////////////////////////////////////////////////////
-	$("#jqGrid").jqGrid('navGrid', '#jqGridPager', {
-		view: false, edit: false, add: false, del: false, search: false,
-		beforeRefresh: function () {
-			refreshGrid("#jqGrid", urlParam);
-		},
-	}).jqGrid('navButtonAdd', "#jqGridPager", {
-		caption: "", cursor: "pointer", position: "first",
-		buttonicon: "glyphicon glyphicon-trash",
-		title: "Delete Selected Row",
-		onClickButton: function () {
-			oper = 'del';
-			selRowId = $("#jqGrid").jqGrid('getGridParam', 'selrow');
-			if (!selRowId) {
-				alert('Please select row');
-				return emptyFormdata(errorField, '#formdata');
-				//return emptyFormdata('#formdata');
-			} else {
-				saveFormdata("#jqGrid", "#dialogForm", "#formdata", 'del', saveParam, urlParam, { 'idno': selrowData('#jqGrid').idno });
+	//////////////////////////My edit options /////////////////////////////////////////////////////////
+			var myEditOptions = {
+				keys: true,
+				extraparam:{
+					"_token": $("#_token").val()
+				},
+				oneditfunc: function (rowid) {
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").hide();
+
+					dialog_regioncode.on();
+
+					$("input[name='recstatus']").keydown(function(e) {//when click tab at last column in header, auto save
+						var code = e.keyCode || e.which;
+						if (code == '9')$('#jqGrid_ilsave').click();
+						/*addmore_jqgrid.state = true;
+						$('#jqGrid_ilsave').click();*/
+					});
+
+				},
+				aftersavefunc: function (rowid, response, options) {
+					if(addmore_jqgrid.state == true)addmore_jqgrid.more=true; //only addmore after save inline
+					//state true maksudnyer ada isi, tak kosong
+					refreshGrid('#jqGrid',urlParam,'add');
+					errorField.length=0;
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").show();
+				},
+				errorfunc: function(rowid,response){
+					alert(response.responseText);
+					refreshGrid('#jqGrid',urlParam,'add');
+				},
+				beforeSaveRow: function (options, rowid) {
+					if(errorField.length>0)return false;
+
+					let data = $('#jqGrid').jqGrid ('getRowData', rowid);
+					console.log(data);
+
+					let editurl = "/unit/form?"+
+						$.param({
+							action: 'unit_save',
+						});
+					$("#jqGrid").jqGrid('setGridParam', { editurl: editurl });
+				},
+				afterrestorefunc : function( response ) {
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").show();
+				},
+				errorTextFormat: function (data) {
+					alert(data);
+				}
+			};
+
+			var myEditOptions_edit = {
+				keys: true,
+				extraparam:{
+					"_token": $("#_token").val()
+				},
+				oneditfunc: function (rowid) {
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").hide();
+
+					dialog_regioncode.on();
+
+					$("input[name='sectorcode']").attr('disabled','disabled');
+					$("input[name='description']").keydown(function(e) {//when click tab at last column in header, auto save
+						var code = e.keyCode || e.which;
+						if (code == '9')$('#jqGrid_ilsave').click();
+						/*addmore_jqgrid.state = true;
+						$('#jqGrid_ilsave').click();*/
+					});
+
+				},
+				aftersavefunc: function (rowid, response, options) {
+					if(addmore_jqgrid.state == true)addmore_jqgrid.more=true; //only addmore after save inline
+					//state true maksudnyer ada isi, tak kosong
+					refreshGrid('#jqGrid',urlParam,'add');
+					errorField.length=0;
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").show();
+				},
+				errorfunc: function(rowid,response){
+					alert(response.responseText);
+					refreshGrid('#jqGrid',urlParam2,'add');
+				},
+				beforeSaveRow: function (options, rowid) {
+					console.log(errorField)
+					if(errorField.length>0)return false;
+
+					let data = $('#jqGrid').jqGrid ('getRowData', rowid);
+					// console.log(data);
+
+					let editurl = "/unit/form?"+
+						$.param({
+							action: 'unit_save',
+						});
+					$("#jqGrid").jqGrid('setGridParam', { editurl: editurl });
+				},
+				afterrestorefunc : function( response ) {
+					$("#jqGridPagerDelete,#jqGridPagerRefresh").show();
+				},
+				errorTextFormat: function (data) {
+					alert(data);
+				}
+			};
+
+			///////////////////////////////////////cust_rules//////////////////////////////////////////////
+			function cust_rules(value,name){
+				var temp;
+				switch(name){
+					case 'Section':temp=$('#regioncode');break;
+				}
+				return(temp.hasClass("error"))?[false,"Please enter valid "+name+" value"]:[true,''];
 			}
-		},
-	}).jqGrid('navButtonAdd', "#jqGridPager", {
-		caption: "", cursor: "pointer", position: "first",
-		buttonicon: "glyphicon glyphicon-info-sign",
-		title: "View Selected Row",
-		onClickButton: function () {
-			oper = 'view';
-			selRowId = $("#jqGrid").jqGrid('getGridParam', 'selrow');
-			populateFormdata("#jqGrid", "#dialogForm", "#formdata", selRowId, 'view');
-		},
-	}).jqGrid('navButtonAdd', "#jqGridPager", {
-		caption: "", cursor: "pointer", position: "first",
-		buttonicon: "glyphicon glyphicon-edit",
-		title: "Edit Selected Row",
-		onClickButton: function () {
-			oper = 'edit';
-			selRowId = $("#jqGrid").jqGrid('getGridParam', 'selrow');
-			populateFormdata("#jqGrid", "#dialogForm", "#formdata", selRowId, 'edit');
-			recstatusDisable();
-		},
-	}).jqGrid('navButtonAdd', "#jqGridPager", {
-		caption: "", cursor: "pointer", position: "first",
-		buttonicon: "glyphicon glyphicon-plus",
-		title: "Add New Row",
-		onClickButton: function () {
-			oper = 'add';
-			$("#dialogForm").dialog("open");
-		},
-	});
+
+			function showdetail(cellvalue, options, rowObject){
+				var field,table,case_;
+				switch(options.colModel.name){
+					case 'regioncode':field=['regioncode','description'];table="sysdb.region";case_='regioncode';break;
+					
+				}
+				var param={action:'input_check',url:'/util/get_value_default',table_name:table,field:field,value:cellvalue,filterCol:[field[0]],filterVal:[cellvalue]};
+
+				fdl.get_array('unit',options,param,case_,cellvalue);
+				
+				return cellvalue;
+			}
+
+			function unitCustomEdit(val, opt) {
+				val = (val == "undefined") ? "" : val.slice(0, val.search("[<]"));
+				return $('<div class="input-group"><input jqgrid="jqGrid" optid="'+opt.id+'" id="'+opt.id+'" name="regioncode" type="text" class="form-control input-sm" data-validation="required" value="' + val + '" style="z-index: 0"><a class="input-group-addon btn btn-primary"><span class="fa fa-ellipsis-h"></span></a></div><span class="help-block"></span>');
+			}
+
+			function galGridCustomValue (elem, operation, value){
+				if(operation == 'get') {
+					return $(elem).find("input").val();
+				} 
+				else if(operation == 'set') {
+					$('input',elem).val(value);
+				}
+			}
+
+			/////////////////////////start grid pager/////////////////////////////////////////////////////////
+			$("#jqGrid").inlineNav('#jqGridPager', {
+				add: true,
+				edit: true,
+				cancel: true,
+				//to prevent the row being edited/added from being automatically cancelled once the user clicks another row
+				restoreAfterSelect: false,
+				addParams: {
+					addRowParams: myEditOptions
+				},
+				editParams: myEditOptions_edit
+			}).jqGrid('navButtonAdd', "#jqGridPager", {
+				id: "jqGridPagerDelete",
+				caption: "", cursor: "pointer", position: "last",
+				buttonicon: "glyphicon glyphicon-trash",
+				title: "Delete Selected Row",
+				onClickButton: function () {
+					selRowId = $("#jqGrid").jqGrid('getGridParam', 'selrow');
+					if (!selRowId) {
+						bootbox.alert('Please select row');
+					} else {
+						bootbox.confirm({
+							message: "Are you sure you want to delete this row?",
+							buttons: {
+								confirm: { label: 'Yes', className: 'btn-success', }, cancel: { label: 'No', className: 'btn-danger' }
+							},
+							callback: function (result) {
+								if (result == true) {
+									param = {
+										_token: $("#_token").val(),
+										action: 'unit_save',
+										regioncode: $('#regioncode').val(),
+										idno: selrowData('#jqGrid').idno,
+									}
+									$.post( "/unit/form?"+$.param(param),{oper:'del'}, function( data ){
+									}).fail(function (data) {
+										//////////////////errorText(dialog,data.responseText);
+									}).done(function (data) {
+										refreshGrid("#jqGrid", urlParam);
+									});
+								}else{
+									$("#jqGridPagerDelete,#jqGridPagerRefresh").show();
+								}
+							}
+						});
+					}
+				},
+			}).jqGrid('navButtonAdd', "#jqGridPager", {
+				id: "jqGridPagerRefresh",
+				caption: "", cursor: "pointer", position: "last",
+				buttonicon: "glyphicon glyphicon-refresh",
+				title: "Refresh Table",
+				onClickButton: function () {
+					refreshGrid("#jqGrid", urlParam);
+				},
+			});
 
 	//////////////////////////////////////end grid/////////////////////////////////////////////////////////
+
 	var dialog_regioncode = new ordialog(
-		'regioncode','sysdb.region','#regioncode',errorField,
+		'regioncode','sysdb.region',"#jqGrid input[name='regioncode']",errorField,
 		{	colModel:[
 				{label:'Region Code',name:'regioncode',width:200,classes:'pointer',canSearch:true,or_search:true},
 				{label:'Description',name:'description',width:400,classes:'pointer',canSearch:true,checked:true,or_search:true},
@@ -153,7 +303,7 @@ $(document).ready(function () {
 							$(gridname+' tr#1').click();
 							$(gridname+' tr#1').dblclick();
 						}else if($(gridname).jqGrid('getDataIDs').length == 0 && obj.ontabbing){
-							$('#'+obj.dialogname).dialog('close');
+							$('#'+obj.dialogname).dialog('close');regioncode
 						}
 					}
 		},{
@@ -165,13 +315,14 @@ $(document).ready(function () {
 		},'urlParam', 'radio', 'tab'
 	);
 	dialog_regioncode.makedialog(true);
+
 	//////////handle searching, its radio button and toggle ///////////////////////////////////////////////
 
-	toogleSearch('#sbut1', '#searchForm', 'on');
-	populateSelect('#jqGrid', '#searchForm');
-	searchClick('#jqGrid', '#searchForm', urlParam);
+	//toogleSearch('#sbut1', '#searchForm', 'on');
+	populateSelect2('#jqGrid', '#searchForm');
+	searchClick2('#jqGrid', '#searchForm', urlParam);
 
 	//////////add field into param, refresh grid if needed////////////////////////////////////////////////
 	addParamField('#jqGrid', true, urlParam);
-	addParamField('#jqGrid', false, saveParam, ['idno','compcode','adduser','adddate','upduser','upddate','recstatus', 'computerid', 'ipaddress']);
+	//addParamField('#jqGrid', false, saveParam, ['idno','compcode','adduser','adddate','upduser','upddate','recstatus', 'computerid', 'ipaddress']);
 });
