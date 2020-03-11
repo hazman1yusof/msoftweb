c56e5b9614aa52285309d6b30f9ab4699633c4f0
AmiraAlias <nuramiralias@gmail.com>
2020-03-11 16:57:41 +0800

    categoryfin

--- resources/views/material/CategoryFIN/category.blade.php
+++ resources/views/material/CategoryFIN/category.blade.php
@@ -44,8 +44,8 @@ input.uppercase {
 		    	<div class='col-md-12' style="padding:0 0 15px 0">
             		<table id="jqGrid" class="table table-striped">
             			<input type="hidden" name="idno">
-						<input id="source2" name="source" type="text" value="{{$_GET['source']}}">	
-						<input id="cattype" name="cattype" type="text" value="{{$_GET['cattype']}}">
+						<input id="source2" name="source" type="hidden" value="{{$_GET['source']}}">	
+						<input id="cattype" name="cattype" type="hidden" value="{{$_GET['cattype']}}">
 							
 						<input id="stockacct" name="stockacct" type="hidden">
 						<input id="cosacct" name="cosacct" type="hidden">
