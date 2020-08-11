5869765d91e5c0704fb7483557d1e0b7c45502b1
LilyAliyya <lilyaliyya@gmail.com>
2020-08-11 15:31:58 +0800

    doctornote controller

--- app/Http/Controllers/hisdb/DoctorNoteController.php
+++ app/Http/Controllers/hisdb/DoctorNoteController.php
@@ -54,6 +54,63 @@ class DoctorNoteController extends defaultController
 
         try {
 
+            DB::table('hisdb.episode')
+                ->where('mrn','=',$request->mrn_doctorNote)
+                ->where('episno','=',$request->episno_doctorNote)
+                ->where('compcode','=',session('compcode'))
+                ->update([
+                    'remark' => $request->remark,
+                    'diagfinal' => $request->diagfinal,
+                    'lastuser'  => session('username'),
+                    'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                ]);
+
+            DB::table('hisdb.patexam')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'episno' => $request->episno_doctorNote,
+                        'examination' => $request->examination,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+            DB::table('hisdb.pathealth')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'episno' => $request->episno_doctorNote,
+                        'clinicnote' => $request->clinicnote,
+                        'pastmedical' => $request->pastmedical,
+                        'social' => $request->social,
+                        'fmh' => $request->fmh,
+                        'plan_' => $request->plan_,
+                        'height' => $request->height,
+                        'weight' => $request->weight,
+                        'bp' => $request->bp,
+                        'pulse' => $request->pulse,
+                        'temperature' => $request->temperature,
+                        'respiration' => $request->respiration,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+            DB::table('hisdb.pathistory')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'drug' => $request->drug,
+                        'alllergyhistory' => $request->alllergyhistory,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
             DB::commit();
 
         } catch (\Exception $e) {
@@ -68,6 +125,119 @@ class DoctorNoteController extends defaultController
         DB::beginTransaction();
 
         try {
+
+            DB::table('hisdb.episode')
+                ->where('mrn','=',$request->mrn_doctorNote)
+                ->where('episno','=',$request->episno_doctorNote)
+                ->where('compcode','=',session('compcode'))
+                ->update([
+                    'remark' => $request->remark,
+                    'diagfinal' => $request->diagfinal,
+                    'lastuser'  => session('username'),
+                    'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                ]);
+
+            $patexam = DB::table('hisdb.patexam')
+                ->where('mrn','=',$request->mrn_doctorNote)
+                ->where('episno','=',$request->episno_doctorNote)
+                ->where('compcode','=',session('compcode'));
+
+            $pathealth = DB::table('hisdb.pathealth')
+                ->where('mrn','=',$request->mrn_doctorNote)
+                ->where('episno','=',$request->episno_doctorNote)
+                ->where('compcode','=',session('compcode'));
+
+            $pathistory = DB::table('hisdb.pathistory')
+                ->where('mrn','=',$request->mrn_doctorNote)
+                ->where('compcode','=',session('compcode'));
+
+            if($patexam->exists() && $pathealth->exists() && $pathistory->exists()){
+                DB::table('hisdb.patexam')
+                    ->where('mrn','=',$request->mrn_doctorNote)
+                    ->where('episno','=',$request->episno_doctorNote)
+                    ->where('compcode','=',session('compcode'))
+                    ->update([
+                        'examination' => $request->examination,
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+                DB::table('hisdb.pathealth')
+                    ->where('mrn','=',$request->mrn_doctorNote)
+                    ->where('episno','=',$request->episno_doctorNote)
+                    ->where('compcode','=',session('compcode'))
+                    ->update([
+                        'clinicnote' => $request->clinicnote,
+                        'pastmedical' => $request->pastmedical,
+                        'social' => $request->social,
+                        'fmh' => $request->fmh,
+                        'plan_' => $request->plan_,
+                        'height' => $request->height,
+                        'weight' => $request->weight,
+                        'bp' => $request->bp,
+                        'pulse' => $request->pulse,
+                        'temperature' => $request->temperature,
+                        'respiration' => $request->respiration,
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+                DB::table('hisdb.pathistory')
+                    ->where('mrn','=',$request->mrn_doctorNote)
+                    ->where('compcode','=',session('compcode'))
+                    ->update([
+                        'drug' => $request->drug,
+                        'alllergyhistory' => $request->alllergyhistory,
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+            }else{
+                DB::table('hisdb.patexam')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'episno' => $request->episno_doctorNote,
+                        'examination' => $request->examination,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+                DB::table('hisdb.pathealth')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'episno' => $request->episno_doctorNote,
+                        'clinicnote' => $request->clinicnote,
+                        'pastmedical' => $request->pastmedical,
+                        'social' => $request->social,
+                        'fmh' => $request->fmh,
+                        'plan_' => $request->plan_,
+                        'height' => $request->height,
+                        'weight' => $request->weight,
+                        'bp' => $request->bp,
+                        'pulse' => $request->pulse,
+                        'temperature' => $request->temperature,
+                        'respiration' => $request->respiration,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+
+                DB::table('hisdb.pathistory')
+                    ->insert([
+                        'compcode' => session('compcode'),
+                        'mrn' => $request->mrn_doctorNote,
+                        'drug' => $request->drug,
+                        'alllergyhistory' => $request->alllergyhistory,
+                        'adduser'  => session('username'),
+                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                        'lastuser'  => session('username'),
+                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    ]);
+            }
 
             $queries = DB::getQueryLog();
             // dump($queries);
