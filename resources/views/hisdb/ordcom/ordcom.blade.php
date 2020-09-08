ea4f84ffe5a38996ec1a9723184468b2971301be
Athiah23 <athiah.work@gmail.com>
2020-09-08 10:38:21 +0800

    ordcom

--- resources/views/hisdb/ordcom/ordcom.blade.php
+++ resources/views/hisdb/ordcom/ordcom.blade.php
@@ -1,26 +1,27 @@
 <div class="panel panel-default" style="position: relative;" id="jqGrid_ordcom_c">
-	<div class="btn-group btn-group-sm pull-right" role="group" aria-label="..." 
-		id="btn_grp_edit_ordcom"
-		style="position: absolute;
-				padding: 0 0 0 0;
-				right: 40px;
-				top: 15px;" 
 
-	>
+<div class="panel-heading clearfix collapsed position" id="toggle_ordcom" style="position: sticky;top: 0px;z-index: 3;">
+		<b>Name: <span id="name_show_ordcom"></span></b><br>
+		MRN: <span id="mrn_show_ordcom"></span>
+
+		<i class="arrow fa fa-angle-double-up" style="font-size:24px;margin: 0 0 0 12px" data-toggle="collapse" data-target="#jqGrid_ordcom_panel"></i>
+		<i class="arrow fa fa-angle-double-down" style="font-size:24px;margin: 0 0 0 12px" data-toggle="collapse" data-target="#jqGrid_ordcom_panel"></i>
+		<div class="pull-right" style="position: absolute; padding: 0 0 0 0; right: 110px; top: 15px;">
+			<h5>Order Communication Detail</h5>
+		</div>
+		<div class="btn-group btn-group-sm pull-right" role="group" aria-label="..." 
+			id="btn_grp_edit_ordcom"
+			style="position: absolute;
+					padding: 0 0 0 0;
+					right: 40px;
+					top: 15px;" 
+
+		>
 		<button type="button" class="btn btn-default" id="new_ordcom">
 			<span class="fa fa-plus-square-o"></span> Order
 		</button>
 	</div>
-	<div class="panel-heading clearfix collapsed position" id="toggle_ordcom" data-toggle="collapse" data-target="#jqGrid_ordcom_panel">
-		<b>Name: <span id="name_show_ordcom"></span></b><br>
-		MRN: <span id="mrn_show_ordcom"></span>
 
-		<i class="arrow fa fa-angle-double-up" style="font-size:24px;margin: 0 0 0 12px"></i>
-		<i class="arrow fa fa-angle-double-down" style="font-size:24px;margin: 0 0 0 12px"></i>
-		<div class="pull-right" style="position: absolute; padding: 0 0 0 0; right: 310px; top: 15px;">
-			<h5>Order Communication Detail</h5>
-		</div>				
-	</div>
 	<div id="jqGrid_ordcom_panel" class="panel-collapse collapse">
 		<div class="panel-body">
 			<div class='col-md-12' style="padding:0 0 15px 0">
