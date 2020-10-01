753a7d361f0ae01bd78c8876cd9811ff80232123
AmiraAlias <nuramiralias@gmail.com>
2020-10-01 15:59:56 +0800

    product

--- resources/views/material/product/product.blade.php
+++ resources/views/material/product/product.blade.php
@@ -63,7 +63,7 @@
 		</form>
 
 		<div class="panel panel-default">
-		    <div class="panel-heading">Charge Master Header</div>
+		    <div class="panel-heading">Product Header</div>
 		    <div class="panel-body">
 		    	<div class='col-md-12' style="padding:0 0 15px 0">
 	        		<table id="jqGrid" class="table table-striped"></table>
