e4b0dfe4ed6108ae3a8677dcfd57369ef37ff559
Athiah23 <athiah.work@gmail.com>
2021-02-22 12:12:07 +0800

    fa

--- resources/views/finance/FA/assetenquiry/assetenquiry.blade.php
+++ resources/views/finance/FA/assetenquiry/assetenquiry.blade.php
@@ -208,7 +208,7 @@
 
 				<div class="form-group">
 					<label class="col-md-2 control-label" for="description">Description</label>  
-						<div class="col-md-3">
+						<div class="col-md-6">
 							<textarea class="form-control input-sm text-uppercase" name="description" rows="3" cols="55" id="description"></textarea>
 						</div>
 				</div>
