d4aae128dda091462d07977a444e93742a3facad
Athiah23 <athiah.work@gmail.com>
2022-01-06 11:41:25 +0800

    icd mma filter sysparam pvalue1

--- app/Http/Controllers/setup/mmamaintenanceController.php
+++ app/Http/Controllers/setup/mmamaintenanceController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 
 class mmamaintenanceController extends defaultController
 {   
+
     var $table;
     var $duplicateCode;
 
@@ -23,6 +24,68 @@ class mmamaintenanceController extends defaultController
     public function show(Request $request)
     {   
         return view('setup.mmamaintenance.mmamaintenance');
+    }
+
+    public function table(Request $request)
+    {   
+        $mmaver = DB::table('sysdb.sysparam')
+                        ->select('pvalue1')
+                        ->where('compcode','=',session('compcode'))
+                        ->where('source','=','MR')
+                        ->where('trantype','=','MMAVER')
+                        ->first();
+
+        $table = DB::table('hisdb.mmamaster')
+                    ->where('version','=',$mmaver->pvalue1)
+                    ->orderBy('idno','asc');
+
+        if(!empty($request->searchCol)){
+            $searchCol_array = $request->searchCol;
+
+            $count = array_count_values($searchCol_array);
+            // dump($count);
+
+            foreach ($count as $key => $value) {
+                $occur_ar = $this->index_of_occurance($key,$searchCol_array);
+
+                $table = $table->where(function ($table) use ($request,$searchCol_array,$occur_ar) {
+                    foreach ($searchCol_array as $key => $value) {
+                        $found = array_search($key,$occur_ar);
+                        if($found !== false){
+                            $table->Where($searchCol_array[$key],'like',$request->searchVal[$key]);
+                        }
+                    }
+                });
+            }
+        }
+
+        //////////paginate/////////
+        $paginate = $table->paginate($request->rows);
+
+        foreach ($paginate->items() as $key => $value) {//ini baru
+            $value->description = $value->description;
+            if(mb_strlen($value->description)>120){
+
+                $time = time() + $key;
+
+                $value->description = mb_substr($value->description,0,120).'<span id="dots_'.$time.'" style="display: inline;"> ... </span><span id="more_'.$time.'" style="display: none;">'.mb_substr($value->description,120).'</span><a id="moreBtn_'.$time.'" style="color: #337ab7 !important;" >Read more</a>';
+
+                $value->callback_param = [
+                    'dots_'.$time,'more_'.$time,'moreBtn_'.$time
+                ];
+            }
+            
+        }
+
+        $responce = new stdClass();
+        $responce->page = $paginate->currentPage();
+        $responce->total = $paginate->lastPage();
+        $responce->records = $paginate->total();
+        $responce->rows = $paginate->items();
+        $responce->sql = $table->toSql();
+        $responce->sql_bind = $table->getBindings();
+
+        return json_encode($responce);
     }
 
     public function form(Request $request)
@@ -45,7 +108,7 @@ class mmamaintenanceController extends defaultController
         try {
 
             $mmamaster = DB::table('hisdb.mmamaster')
-                            ->where('mmacode','=',$request->mmacode);
+                            ->where('mmacode','=',strtoupper($request->mmacode));
 
             $type = DB::table('sysdb.sysparam')
                             ->where('source','=',"MR")
@@ -65,20 +128,33 @@ class mmamaintenanceController extends defaultController
                     'recstatus' => strtoupper($request->recstatus),
                     //'idno' => strtoupper($request->idno),
                     'lastcomputerid' => strtoupper($request->lastcomputerid),
-                    'lastipaddress' => strtoupper($request->lastipaddress),
-                    'adduser' => strtoupper(session('username')),
-                    'adddate' => Carbon::now("Asia/Kuala_Lumpur")
+                    'lastuser' => session('username'),
+                    'lastupdate' => Carbon::now("Asia/Kuala_Lumpur")
                 ]);
 
              DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
 
-            $responce = new stdClass();
-            $responce->errormsg = $e->getMessage();
-            $responce->request = $_REQUEST;
+            return response('Error'.$e, 500);
+        }
 
-            return response(json_encode($responce), 500);        }
+        //////////paginate/////////
+        $paginate = $table->paginate($request->rows);
+
+        foreach ($paginate->items() as $key => $value) {//ini baru
+            $value->remarks_show = $value->remarks;
+            if(mb_strlen($value->remarks)>120){
+        
+                $time = time() + $key;
+        
+                $value->remarks_show = mb_substr($value->remarks_show,0,120).'<span id="dots_'.$time.'" style="display: inline;">...</span><span id="more_'.$time.'" style="display: none;">'.mb_substr($value->remarks_show,120).'</span><a id="moreBtn_'.$time.'" style="color: #337ab7 !important;" >Read more</a>';
+        
+                $value->callback_param = [
+                    'dots_'.$time,'more_'.$time,'moreBtn_'.$time
+                ];
+            }
+        }
     }
 
     public function edit(Request $request){
@@ -86,23 +162,18 @@ class mmamaintenanceController extends defaultController
         DB::beginTransaction();
         try {
 
-            $type = DB::table('sysdb.sysparam')
-            ->where('source','=',"MR")
-            ->where('trantype','=',"MMAVER")
-            ->first();
-
             DB::table('hisdb.mmamaster')
                 ->where('idno','=',$request->idno)
                 ->update([  
-                    'mmacode' => strtoupper($request->mmacode),
+                    //'mmacode' => strtoupper($request->mmacode),
                     'description' => strtoupper($request->description),
                     'version' => $type->pvalue1,
                     'recstatus' => strtoupper($request->recstatus),
-                    'idno' => $request->idno,
+                    'idno' => strtoupper($request->idno),
                     'lastcomputerid' => strtoupper($request->lastcomputerid),
                     'lastipaddress' => strtoupper($request->lastipaddress),
-                    'lastuser' => strtoupper(session('username')),
-                    'lastupdate' => Carbon::now("Asia/Kuala_Lumpur")
+                    'upduser' => strtoupper(session('username')),
+                    'upddate' => Carbon::now("Asia/Kuala_Lumpur")
                 ]); 
 
             DB::commit();
--- public/js/setup/icd/icd.js
+++ public/js/setup/icd/icd.js
@@ -1,3 +1,4 @@
+///////////////////////////icd.js////////////////////////////////////
 $.jgrid.defaults.responsive = true;
 $.jgrid.defaults.styleUI = 'Bootstrap';
 var editedRow=0;
@@ -29,11 +30,16 @@ $(document).ready(function () {
 	/////////////////////parameter for jqgrid url/////////////////////////////////////////////////
     var urlParam={
         action:'get_table_default',
-        url:'util/get_table_default',
-        field:'',
-        table_name:'hisdb.diagtab',
-        table_id:'icdcode',
-        sort_idno: true    
+        url:'icd/table',
+        // field:'',
+		// table_name:['hisdb.diagtab AS dt', 'sysdb.sysparam AS sp'],
+		// table_id:'idno',
+		// join_type:['LEFT JOIN'],
+		// join_onCol:['dt.type'],
+		// join_onVal:['sp.pvalue1'],
+		// filterCol:['dt.type','dt.compcode'],
+		// filterVal:['','session.coompcode'],
+		// sort_idno: true,
 	}
 
 	/////////////////////parameter for saving url////////////////////////////////////////////////
@@ -42,7 +48,7 @@ $(document).ready(function () {
 		datatype: "local",
         editurl: "/icd/form",
 		colModel: [
-             { label: 'idno', name: 'idno', width: 5,hidden:true, key:true},
+            { label: 'idno', name: 'idno', width: 5,hidden:true, key:true},
 			{ label: 'ICD Code', name: 'icdcode', width: 15, canSearch: true, editable: true, editrules: { required: true }, editoptions: {style: "text-transform: uppercase" }},
 			// { label: 'Description', name: 'description_show', classes: 'wrap', width: 80, checked: true, editable: true, edittype: "textarea", editrules: { required: true }, 
 			// 	editoptions: 
