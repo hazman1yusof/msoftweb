b40387ad7a456f38d68d16991a74b70165f757a6
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2022-09-07 18:23:27 +0800

    sds

--- public/js/hisdb/mykadfp/mykadFP.js
+++ public/js/hisdb/mykadfp/mykadFP.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
 		    hideDuration : 200,}
 		}).modal('show');
 
-		$.get( "http://mycard.test/mycard/public/read_mykad", function( data ) {
+		$.get( "http://mycard.test/read_mykad", function( data ) {
 			
 		},'json')
 		  .done(function( data ) {
@@ -132,7 +132,7 @@ $(document).ready(function () {
 		    hideDuration : 200,}
 		}).modal('show');
 
-		$.get( "http://mycard.test/mycard/public/read_mykid", function( data ) {
+		$.get( "http://mycard.test/read_mykid", function( data ) {
 			
 		},'json')
 		  .done(function( data ) {
