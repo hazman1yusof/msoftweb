5e00ff3f9380fe1af889d3bb5a374cfeae50872a
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2023-03-21 10:53:15 +0800

    sdd

--- public/js/finance/AP/creditNote/creditNote.js
+++ public/js/finance/AP/creditNote/creditNote.js
@@ -556,8 +556,8 @@ $(document).ready(function () {
 		}).done(function (data) {
 
 			hideatdialogForm(false);
+			addmore_jqgrid2.state = true;
 			if($('#jqGrid2').jqGrid('getGridParam', 'reccount') < 1){
-				addmore_jqgrid2.state = true;
 				$('#jqGrid2_iladd').click();
 			}
 			
@@ -839,6 +839,8 @@ $(document).ready(function () {
 		    "_token": $("#_token").val()
         },
         oneditfunc: function (rowid) {
+        	$("#jqGrid2").setSelection($("#jqGrid2").getDataIDs()[0]);
+        	errorField.length=0;
 			$("#jqGrid2 input[name='deptcode']").focus().select();
         	$("#jqGridPager2EditAll,#saveHeaderLabel,#jqGridPager2Delete").hide();
 
@@ -899,8 +901,9 @@ $(document).ready(function () {
 			$("#jqGrid2").jqGrid('setGridParam',{editurl:editurl});
         },
         afterrestorefunc : function( response ) {
+        	errorField.length=0;
 			hideatdialogForm(false);
-			$('#jqGrid2').jqGrid ('setSelection', "1");
+			// $('#jqGrid2').jqGrid ('setSelection', "1");
 	    }
     };
 
