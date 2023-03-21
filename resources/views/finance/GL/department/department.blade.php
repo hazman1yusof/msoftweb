beb710b44d3a1654bbbc50fb0f3686aa0d391a69
AmiraAlias <nuramiralias@gmail.com>
2023-03-21 12:55:27 +0800

    dept

--- resources/views/finance/GL/department/department.blade.php
+++ resources/views/finance/GL/department/department.blade.php
@@ -184,12 +184,12 @@ input.uppercase {
 		<div class="form-group">
 			<label class="col-md-2 control-label" for="computerid">Computer Id</label>  
 				<div class="col-md-2">
-				  	<input id="computerid" name="computerid" type="text" class="form-control input-sm" data-validation="required" frozeOnEdit hideOne >
+				  	<input id="computerid" name="computerid" type="text" class="form-control input-sm" frozeOnEdit hideOne >
 				</div>
 
 				<label class="col-md-2 control-label" for="lastcomputerid">Last Computer Id</label>  
 				<div class="col-md-2">
-				  	<input id="lastcomputerid" name="lastcomputerid" type="text" class="form-control input-sm" data-validation="required" frozeOnEdit hideOne >
+				  	<input id="lastcomputerid" name="lastcomputerid" type="text" class="form-control input-sm" frozeOnEdit hideOne >
 				</div>
 
 		</div> 
@@ -197,13 +197,13 @@ input.uppercase {
 		<div class="form-group">
 		<label class="col-md-2 control-label" for="ipaddress">IP Address</label>  
 				  	<div class="col-md-2">
-						<input id="ipaddress" name="ipaddress" type="text" maxlength="30" class="form-control input-sm" data-validation="required" frozeOnEdit hideOne>
+						<input id="ipaddress" name="ipaddress" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
 				  	</div>
 			
 
 				<label class="col-md-2 control-label" for="lastipaddress">Last IP Address</label>  
 				  	<div class="col-md-2">
-						<input id="lastipaddress" name="lastipaddress" type="text" maxlength="30" class="form-control input-sm" data-validation="required" frozeOnEdit hideOne>
+						<input id="lastipaddress" name="lastipaddress" type="text" maxlength="30" class="form-control input-sm" frozeOnEdit hideOne>
 				  	</div>
 		</div>
 	</form>
