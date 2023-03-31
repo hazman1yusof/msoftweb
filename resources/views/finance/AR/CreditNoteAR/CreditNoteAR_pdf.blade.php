a848529a7f89d42fdf8dfd124ac04a3914e081a2
LilyAliyya <lilyaliyya@gmail.com>
2023-03-31 11:33:15 +0800

    cn pdf

--- resources/views/finance/AR/CreditNoteAR/CreditNoteAR_pdf.blade.php
+++ resources/views/finance/AR/CreditNoteAR/CreditNoteAR_pdf.blade.php
@@ -41,6 +41,10 @@ hr {
     margin-left: 36px;
 }
 
+.tab7 {
+    margin-left: 38px;
+}
+
 @endsection
 
 @section('body')
@@ -118,10 +122,33 @@ hr {
                 <hr>
                 <div class="card">
                     <div class="card-body">
-                        <p>Reference:&nbsp; {{$dbacthdr->reference}}</p>
-                        <p>Remark:&nbsp; {{$dbacthdr->remark}}</p>
-                        <p>Print Date/Time:&nbsp; {{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('d/m/Y')}} {{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('H:i:s')}}</p>
+                        <p>Reference <span style="margin-left: 29px;"> :&nbsp; {{$dbacthdr->reference}}</p>
+                        <p>Remark <span style="margin-left: 38px;"> :&nbsp; {{$dbacthdr->remark}}</p>
+                        <p>Print Date/Time :&nbsp; {{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('d/m/Y')}} {{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('H:i:s')}}</p>
                     </div>
+                </div>
+                <div class="products p-2">
+                    <table class="table table-borderless">
+                        <tbody>
+                            <tr>
+                                <td width="12%">
+                                    <p>Reference</p>
+                                    <p>Remark</p>
+                                    <p>Print Date/Time</p>
+                                </td>
+                                <td width="3%">
+                                    <p>:</p>
+                                    <p>:</p>
+                                    <p>:</p>
+                                </td>
+                                <td width="60%">
+                                    <p>{{$dbacthdr->reference}}</p>
+                                    <p>{{$dbacthdr->remark}}</p>
+                                    <p>{{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('d/m/Y')}} {{\Carbon\Carbon::now("Asia/Kuala_Lumpur")->format('H:i:s')}}</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <!-- </div> -->
