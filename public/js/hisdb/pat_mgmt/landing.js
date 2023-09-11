0303fa1928deeff775869c32ddcfbcb79d7cae02
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2023-09-11 15:58:03 +0800

    asd

--- public/js/hisdb/pat_mgmt/landing.js
+++ public/js/hisdb/pat_mgmt/landing.js
@@ -782,6 +782,17 @@ $(document).ready(function() {
 
 });
 
+function randomString(length, chars) {
+    var mask = '';
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (chars.indexOf('#') > -1) mask += '0123456789';
+    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+    var result = '';
+    for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
+    return result;
+}
+
 var textfield_modal = new textfield_modal();
 textfield_modal.ontabbing();
 textfield_modal.checking();
