fc2d08f9ee70319b906a112912100a19df70c9ac
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2023-09-12 11:00:28 +0800

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
                 ->where('compcode','=',$user->compcode)
                 ->get();
         $unit_user = '';
-        $title="Ambulatory Care";
+        $title="Allied Healthcare";
         if($user->dept != ''){
             $unit_user_ = DB::table('sysdb.department')
                 ->where('compcode','=',$user->compcode)
