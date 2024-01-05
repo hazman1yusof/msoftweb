d0d9350ca45b0f5989a6b9321db944a849a908ac
LilyAliyya <lilyaliyya@gmail.com>
2024-01-05 11:08:12 +0800

    report excel

--- resources/views/finance/AR/paymentAlloc_Report/paymentAlloc_Report.blade.php
+++ resources/views/finance/AR/paymentAlloc_Report/paymentAlloc_Report.blade.php
@@ -18,7 +18,7 @@
 @section('body')
     <div class="container">
         <div class="jumbotron" style="margin-top: 30px;text-align: center;">
-            <form method="get" id="genreport" action="/paymentAlloc_Report/showExcel">
+            <form method="get" id="genreport" action="./paymentAlloc_Report/showExcel">
                 <h2>PAYMENT ALLOCATION LISTING</h2>
                 <h4 style="padding:3% 10% 3% 10%; letter-spacing: 1px;line-height: 1.5"> </h4>
                 
