c021afb275ccee7640edacb08d24d18b5969817c
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-03-12 12:23:29 +0800

    sd

--- app/Http/Controllers/hisdb/DoctorNoteController.php
+++ app/Http/Controllers/hisdb/DoctorNoteController.php
@@ -658,7 +658,7 @@ class DoctorNoteController extends defaultController
             })
             ->where('e.compcode','=',session('compcode'))
             ->where('e.mrn','=',$request->mrn)
-            ->orderBy('p.adddate','desc');
+            ->orderBy('p.adddate','asc');
         
         // $patexam_obj = DB::table('hisdb.pathealth')
         //     ->select('mrn','episno','recordtime','adddate','adduser')
