169fad286e64424e902037f50b73827b94d1dd80
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-03-17 14:20:48 +0800

    asd

--- app/Http/Controllers/material/ItemEnquiryController.php
+++ app/Http/Controllers/material/ItemEnquiryController.php
@@ -66,6 +66,7 @@ class ItemEnquiryController extends defaultController
                 ->where('d.trandate','<=',$request->trandate_to)
                 ->where('d.amount','!=',0)
                 ->orderBy('d.trandate', 'asc')
+                ->orderBy('d.trantime', 'asc')
                 ->get();
 
         $det_mov_deptcode = $det_mov_deptcode->each(function ($item, $key) {
@@ -89,6 +90,7 @@ class ItemEnquiryController extends defaultController
             ->where('d.trandate','<=',$request->trandate_to)
             ->where('d.amount','!=',0)
             ->orderBy('d.trandate', 'asc')
+            ->orderBy('d.trantime', 'asc')
             ->get();
 
         $det_mov_deptcode_ivdspdt = $det_mov_deptcode_ivdspdt->each(function ($item, $key) {
@@ -116,6 +118,7 @@ class ItemEnquiryController extends defaultController
                 ->where('d.trandate','<=',$request->trandate_to)
                 ->where('d.amount','!=',0)
                 ->orderBy('d.trandate', 'asc')
+                ->orderBy('d.trantime', 'asc')
                 ->get();
 
         $det_mov_sndrcv = $det_mov_sndrcv->each(function ($item, $key) {
