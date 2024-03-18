369ba4e949398f163f97e0ad0677e7564024aec5
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-03-18 12:36:56 +0800

    asd

--- resources/views/finance/AR/ARAgeing_Report/ARAgeing_Report_pdfmake.blade.php
+++ resources/views/finance/AR/ARAgeing_Report/ARAgeing_Report_pdfmake.blade.php
@@ -64,7 +64,7 @@
                 pageSize: 'A4',
                 content: [
                     {
-                        image: 'letterhead', width: 400, height: 80, style: 'tableHeader', colSpan: 5, alignment: 'center'
+                        image: 'letterhead', width: 200, height: 40, style: 'tableHeader', colSpan: 5, alignment: 'center'
                     },
                     {
                         text: '\n{{$title}}\n',
