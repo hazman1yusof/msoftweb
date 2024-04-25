551f06c665b8f3b7220467a77e27c33cc19506f2
AmiraAlias <nuramiralias@gmail.com>
2024-04-25 09:48:43 +0800

    repack

---
+++ app/Http/Controllers/material/RepackDetailController.php
@@ -0,0 +1,385 @@
+<?php
+
+namespace App\Http\Controllers\material;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\defaultController;
+use stdClass;
+use DB;
+use Carbon\Carbon;
+
+class RepackDetailController extends defaultController
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function table(Request $request)
+    {
+        switch($request->action){
+            case 'get_table_dtl':
+                return $this->get_table_dtl($request);
+            case 'get_deptcodedtl':
+                return $this->get_deptcodedtl($request);
+            case 'get_itemcodedtl':
+                return $this->get_itemcodedtl($request);
+            default:
+                return 'error happen..';
+        }
+    }
+    
+    public function form(Request $request)
+    {
+        switch($request->oper){
+            case 'add':
+                return $this->add($request);
+            case 'edit':
+                return $this->edit($request);
+            case 'edit_all':
+                return $this->edit_all($request);
+            case 'del':
+                return $this->del($request);
+            default:
+                return 'error happen..';
+        }
+    }
+    
+    public function chgDate($date){
+        
+        if(!empty($date)){
+            $newstr=explode("/", $date);
+            return $newstr[2].'-'.$newstr[1].'-'.$newstr[0];
+        }else{
+            return '0000-00-00';
+        }
+        
+    }
+    
+    public function get_table_dtl(Request $request){
+        
+        $table = DB::table('material.repackdt')
+                ->where('recno','=',$request->recno)
+                ->where('compcode','=',session('compcode'));
+
+        if(!empty($request->sidx)){
+            if(!empty($request->fixPost)){
+                $request->sidx = substr_replace($request->sidx, ".", strpos($request->sidx, "_"), strlen("."));
+            }
+            
+            $pieces = explode(", ", $request->sidx .' '. $request->sord);
+            if(count($pieces)==1){
+                $table = $table->orderBy($request->sidx, $request->sord);
+            }else{
+                for ($i = sizeof($pieces)-1; $i >= 0 ; $i--) {
+                    $pieces_inside = explode(" ", $pieces[$i]);
+                    $table = $table->orderBy($pieces_inside[0], $pieces_inside[1]);
+                }
+            }
+        }
+
+        //////////paginate//////////
+        $paginate = $table->paginate($request->rows);
+        
+        $responce = new stdClass();
+        $responce->page = $paginate->currentPage();
+        $responce->total = $paginate->lastPage();
+        $responce->records = $paginate->total();
+        $responce->rows = $paginate->items();
+        $responce->sql = $table->toSql();
+        $responce->sql_bind = $table->getBindings();
+        
+        return json_encode($responce);
+        
+    }
+    
+    public function add(Request $request){
+        
+        DB::beginTransaction();
+        
+        try {
+            
+            $repackhd = DB::table("material.repackhd")
+                ->where('recno','=',$request->recno)
+                ->where('compcode','=',session('compcode'))
+                ->first();
+
+            $recno = $repackhd->recno;
+            $outqty = $repackhd->outqty;
+
+            dd($recno);
+          
+            ////1. calculate lineno_ by recno
+            $sqlln = DB::table('material.repackdt')->select('lineno_')
+                        ->where('compcode','=',session('compcode'))
+                        ->where('recno','=',$recno)
+                        ->count('lineno_');
+
+            $li=intval($sqlln)+1;
+            
+            // 2. insert detail
+            DB::table('material.repackdt')
+                ->insert([
+                    'compcode' => session('compcode'),
+                    'recno' => $recno,
+                    'lineno_' => $li,
+                    'deptcode' => strtoupper($request->deptcode),
+                    'olditemcode' => $request->olditemcode,
+                    'uomcode' => strtoupper($request->uomcode),
+                    'inpqty' => $request->inpqty,
+                    'avgcost' => floatval($request->avgcost),
+                    'amount' => floatval($request->amount),
+                   // 'trandate' => $request->trandate,
+                    'adduser' => session('username'),
+                    'adddate' => Carbon::now("Asia/Kuala_Lumpur"),
+                    'recstatus' => 'OPEN',
+
+                ]);
+
+            ///3. calculate total amount from detail
+            $totalAmount = DB::table('material.repackdt')
+                    ->where('compcode','=',session('compcode'))
+                    ->where('recno','=',$recno)
+                    ->where('recstatus','!=','DELETE')
+                    ->sum('amount');
+            
+            ///4. then update to header
+            DB::table('material.repackhd')
+                ->where('compcode','=',session('compcode'))
+                ->where('recno','=',$recno)
+                ->update([
+                    'amount' => $totalAmount, 
+                    'avgcost'=> $totalAmount / $outqty,
+                ]);
+
+            $responce = new stdClass();
+            $responce->totalAmount = $totalAmount;
+            $responce->recno = $recno;
+
+            DB::commit();
+            
+        } catch (\Exception $e) {
+            
+            DB::rollback();
+            
+            return response($e->getMessage(), 500);
+            
+        }
+        
+    }
+    
+    public function edit(Request $request){
+        
+        DB::beginTransaction();
+        
+        try {
+            
+            // 1. update detail
+            DB::table('finance.glrptfmt')
+                ->where('compcode','=',session('compcode'))
+                ->where('idno','=',$request->idno)
+                ->update([
+                    'lineno_' => $request->lineno_,
+                    'printflag' => strtoupper($request->printflag),
+                    'rowdef' => $request->rowdef,
+                    'code' => strtoupper($request->code),
+                    'note' => $request->note,
+                    'description' => $request->description,
+                    'formula' => $request->formula,
+                    'costcodefr' => $request->costcodefr,
+                    'costcodeto' => $request->costcodeto,
+                    'revsign' => $request->revsign,
+                    'upduser'=> session('username'),
+                    'upddate'=> Carbon::now("Asia/Kuala_Lumpur"),
+                ]);
+            
+            DB::commit();
+            
+        } catch (\Exception $e) {
+            
+            DB::rollback();
+            
+            return response($e->getMessage(), 500);
+            
+        }
+        
+    }
+    
+    public function edit_all(Request $request){
+        
+        DB::beginTransaction();
+        
+        try {
+            
+            foreach ($request->dataobj as $key => $value) {
+                
+                // 1. update detail
+                DB::table('finance.glrptfmt')
+                    ->where('compcode','=',session('compcode'))
+                    ->where('idno','=',$value['idno'])
+                    ->update([
+                        'lineno_' => $value['lineno_'],
+                        'printflag' => strtoupper($value['printflag']),
+                        'rowdef' => $value['rowdef'],
+                        'code' => strtoupper($value['code']),
+                        'note' => $value['note'],
+                        'description' => $value['description'],
+                        'formula' => $value['formula'],
+                        'costcodefr' => $value['costcodefr'],
+                        'costcodeto' => $value['costcodeto'],
+                        'revsign' => $value['revsign'],
+                        'upduser' => session('username'),
+                        'upddate' => Carbon::now("Asia/Kuala_Lumpur"),
+                    ]);
+            
+            }
+            
+            DB::commit();
+            
+        } catch (\Exception $e) {
+            
+            DB::rollback();
+            
+            return response('Error'.$e, 500);
+            
+        }
+        
+    }
+    
+    public function del(Request $request){
+        
+        DB::beginTransaction();
+        
+        try {
+            
+            DB::table('finance.glrptfmt')
+                ->where('compcode','=',session('compcode'))
+                ->where('idno','=',$request->idno)
+                ->delete();
+            
+            DB::commit();
+            
+        } catch (\Exception $e) {
+            
+            DB::rollback();
+            
+            return response($e->getMessage(), 500);
+            
+        }
+        
+    }
+
+    public function get_deptcodedtl(Request $request){
+
+
+        $table = DB::table('material.stockloc as s')
+                    ->select('s.deptcode as s_deptcode', 'd.description as d_description')
+                    ->join('sysdb.department as d', function($join){
+                        $join = $join->on('d.deptcode', '=', 's.deptcode')
+                                    ->where('d.compcode', '=', session('compcode'));
+                    })
+                    ->where('s.recstatus','=','ACTIVE')
+                    ->where('s.compcode','=',session('compcode'))
+                    ->where('s.year', '=', $request->filterVal[2])
+                    ->whereNotNull('s.deptcode')
+                    ->where('s.deptcode','<>','')
+                    ->distinct('s.deptcode');
+
+        // $table = $table->get(['s.deptcode', 'd.deptcode', 'd.description']);
+
+        //dd($table);
+
+        if(!empty($request->searchCol)){
+            $searchCol_array = $request->searchCol;
+            
+            $count = array_count_values($searchCol_array);
+            // dump($count);
+            
+            foreach ($count as $key => $value) {
+                $occur_ar = $this->index_of_occurance($key,$searchCol_array);
+                
+                $table = $table->where(function ($table) use ($request,$searchCol_array,$occur_ar) {
+                    foreach ($searchCol_array as $key => $value) {
+                        $found = array_search($key,$occur_ar);
+                        if($found !== false){
+                            $table->Where($searchCol_array[$key],'like',$request->searchVal[$key]);
+                        }
+                    }
+                });
+            }
+        }
+        
+        $paginate = $table->paginate($request->rows);
+        
+        $responce = new stdClass();
+        $responce->page = $paginate->currentPage();
+        $responce->total = $paginate->lastPage();
+        $responce->records = $paginate->total();
+        $responce->rows = $paginate->items();
+        $responce->sql = $table->toSql();
+        $responce->sql_bind = $table->getBindings();
+        $responce->sql_query = $this->getQueries($table);
+        
+        return json_encode($responce);
+        
+    }
+
+    public function get_itemcodedtl(Request $request){
+
+
+        $table = DB::table('material.stockloc as s')
+                    ->select('s.itemcode as s_itemcode', 'p.description as p_description', 's.uomcode as s_uomcode', 'p.uomcode as p_uomcode', 'p.avgcost as p_avgcost')
+                    ->join('material.product as p', function($join){
+                        $join = $join->on('p.itemcode', '=', 's.itemcode')
+                                    ->where('p.compcode', '=', session('compcode'));
+                    })
+                    ->where('s.recstatus','=','ACTIVE')
+                    ->where('s.compcode','=',session('compcode'))
+                    ->where('s.deptcode', '=', $request->filterVal[0])
+                    ->where('s.year', '=', $request->filterVal[3])
+                    // ->whereNotNull('s.deptcode')
+                    // ->where('s.deptcode','<>','')
+                    ->distinct('s.itemcode');
+
+        // $table = $table->get(['s.deptcode', 'd.deptcode', 'd.description']);
+
+        //dd($table);
+
+        if(!empty($request->searchCol)){
+            $searchCol_array = $request->searchCol;
+            
+            $count = array_count_values($searchCol_array);
+            // dump($count);
+            
+            foreach ($count as $key => $value) {
+                $occur_ar = $this->index_of_occurance($key,$searchCol_array);
+                
+                $table = $table->where(function ($table) use ($request,$searchCol_array,$occur_ar) {
+                    foreach ($searchCol_array as $key => $value) {
+                        $found = array_search($key,$occur_ar);
+                        if($found !== false){
+                            $table->Where($searchCol_array[$key],'like',$request->searchVal[$key]);
+                        }
+                    }
+                });
+            }
+        }
+        
+        $paginate = $table->paginate($request->rows);
+        
+        $responce = new stdClass();
+        $responce->page = $paginate->currentPage();
+        $responce->total = $paginate->lastPage();
+        $responce->records = $paginate->total();
+        $responce->rows = $paginate->items();
+        $responce->sql = $table->toSql();
+        $responce->sql_bind = $table->getBindings();
+        $responce->sql_query = $this->getQueries($table);
+        
+        return json_encode($responce);
+        
+    }
+    
+}
+
---
+++ resources/views/material/repack/repack.blade.php
@@ -0,0 +1,128 @@
+@extends('layouts.main')
+
+@section('title', 'Repack')
+
+@section('style')
+    .panel-heading.collapsed .fa-angle-double-up,
+    .panel-heading .fa-angle-double-down {
+        display: none;
+    }
+    
+    .panel-heading.collapsed .fa-angle-double-down,
+    .panel-heading .fa-angle-double-up {
+        display: inline-block;
+    }
+    
+    i.fa {
+        cursor: pointer;
+        float: right;
+        <!-- margin-right: 5px; -->
+    }
+    
+    .collapsed ~ .panel-body {
+        padding: 0;
+    }
+    
+    .clearfix {
+        overflow: auto;
+    }
+@endsection
+
+@section('body')
+    <input id="_token" name="_token" type="hidden" value="{{ csrf_token() }}">
+    
+    <!--***************************** Search + table *****************************-->
+    <div class='row'>
+        <form id="searchForm" class="formclass" style='width:99%; position:relative' onkeydown="return event.key != 'Enter';">
+            <fieldset>
+                <input id="getYear" name="getYear" type="hidden"  value="{{Carbon\Carbon::now()->year}}">
+                
+                <div class='col-md-12' style="padding:0 0 15px 0;">
+                    <div class="form-group">
+                        <div class="col-md-2">
+                            <label class="control-label" for="Scol">Search By : </label>
+                            <select id='Scol' name='Scol' class="form-control input-sm" tabindex="1"></select>
+                        </div>
+                        
+                        <div class="col-md-5" style="margin-top: 4px;">
+							<label class="control-label"></label>
+							<input name="Stext" type="search" seltext='true' placeholder="Search here ..." class="form-control text-uppercase" tabindex="2">
+						</div>
+		            </div>
+				</div>
+            </fieldset>
+        </form>
+        
+        <div class="panel panel-default">
+            <div class="panel-heading"> Repack Input
+                <!-- <a class='pull-right pointer text-primary' id='pdfgen1' href="" target="_blank"><span class='fa fa-print'></span> Print </a> -->
+            </div>
+            <div class="panel-body">
+                <div class='col-md-12' style="padding:0 0 15px 0">
+                    <table id="jqGrid" class="table table-striped"></table>
+                    <div id="jqGridPager"></div>
+                </div>
+            </div>
+        </div>
+		
+		<div class="panel-group">
+			<div class="panel panel-default" id="jqGrid2_c">
+				<div class="panel-heading clearfix collapsed" data-toggle="collapse" href="#jqGrid2_panel">
+					<i class="fa fa-angle-double-up" style="font-size:24px"></i><i class="fa fa-angle-double-down" style="font-size:24px"></i>Repack Output
+				</div>
+				<div id="jqGrid2_panel" class="panel-collapse collapse">
+					<div class="panel-body">
+						<form id='formdata2' class='form-vertical' style='width:99%'>
+							<label class="col-md-3 control-label" for="grandtot" style="float:right;">
+								Grand Total: <input type="input" name="grandtot" id="grandtot" disabled="disabled">
+							</label> 
+							<div class='col-md-12' style="padding:0 0 15px 0">
+								<table id="jqGrid2" class="table table-striped"></table>
+								<div id="jqGridPager2"></div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!--***************************** End Search + table *****************************-->
+@endsection
+
+@section('scripts')
+	<script type="text/javascript">
+		$(document).ready(function () {
+			if(!$("table#jqGrid").is("[tabindex]")){
+				$("#jqGrid").bind("jqGridGridComplete", function () {
+					$("table#jqGrid").attr('tabindex', 2);
+					$("td#input_jqGridPager input.ui-pg-input.form-control").attr('tabindex', 3);
+					$("td#input_jqGridPager input.ui-pg-input.form-control").on('focus',onfocus_pageof);
+					if($('table#jqGrid').data('enter')){
+						$('td#input_jqGridPager input.ui-pg-input.form-control').focus();
+						$("table#jqGrid").data('enter',false);
+					}
+				});
+			}
+			
+			function onfocus_pageof(){
+				$(this).keydown(function(e){
+					var code = e.keyCode || e.which;
+					if (code == '9'){
+						e.preventDefault();
+						$('input[name=Stext]').focus();
+					}
+				});
+				
+				$(this).keyup(function(e) {
+					var code = e.keyCode || e.which;
+					if (code == '13'){
+						$("table#jqGrid").data('enter',true);
+					}
+				});
+			}
+		});
+	</script>
+	<script src="js/material/repack/repack.js"></script>
+	<script src="plugins/pdfmake/pdfmake.min.js"></script>
+	<script src="plugins/pdfmake/vfs_fonts.js"></script>
+@endsection
