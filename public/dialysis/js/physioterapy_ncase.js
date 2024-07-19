69f369934a4ca19f4b07af2cd9cd1416c455142f
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-07-19 17:31:43 +0800

    asd

--- public/dialysis/js/physioterapy_ncase.js
+++ public/dialysis/js/physioterapy_ncase.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
 				let url = $('#urltodiagram').val() + filename;
 				var win = window.open(url, '_blank');
 			}else{
-				var win = window.open('http://localhost:8080/foxitweb/public/pdf?mrn='+mrn+'&episno=&type='+type+'&from=rehab', '_blank');
+				var win = window.open('http://localhost:8443/foxitweb/public/pdf?mrn='+mrn+'&episno=&type='+type+'&from=rehab', '_blank');
 			}
 
 			if (win) {
