1ca8a0291e5d872ac7eff348363dace75bbd7faa
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-09-27 13:59:35 +0800

    asd

--- app/Http/Controllers/material/AuthorizationDtlController.php
+++ app/Http/Controllers/material/AuthorizationDtlController.php
@@ -125,9 +125,9 @@ class AuthorizationDtlController extends defaultController // DONT DELETE THIS C
                             });;
                     })
                     ->where('qpo.compcode',session('compcode'))
-                    ->where('qpo.trantype','<>','DONE');
-        dd($this->getQueries($queuepo));
-                    // ->get();
+                    ->where('qpo.trantype','<>','DONE')
+                    ->get();
+        // dd($this->getQueries($queuepo));
 
         $queuepo_reject = DB::table('material.queuepo as qpo')
                     ->select('qpo.trantype','pohd.recno','pohd.prdept','pohd.cancelby','pohd.canceldate','pohd.recstatus','pohd.totamount','pohd.adduser')
