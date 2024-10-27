d53969b3b66c71e53fd953033026b35915afc393
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2024-10-27 22:11:19 +0800

    ad

--- app/Http/Controllers/finance/SalesOrderController.php
+++ app/Http/Controllers/finance/SalesOrderController.php
@@ -117,6 +117,8 @@ class SalesOrderController extends defaultController
                 return $this->posted($request);
             case 'delivered':
                 return $this->delivered($request);
+            case 'reopen_posted':
+                return $this->reopen_posted($request);
             case 'reject':
                 return $this->reject($request);
             case 'reopen':
@@ -2547,7 +2549,7 @@ class SalesOrderController extends defaultController
 
             if($expdate_obj->exists()){
                 $expdate_first = $expdate_obj->first();
-                $txnqty_ = $curr_quan;
+                // $txnqty_ = $curr_quan;
                 $balqty = floatval($expdate_first->balqty) + floatval($prev_quan);
                 $expdate_obj
                         ->update([
@@ -2555,7 +2557,7 @@ class SalesOrderController extends defaultController
                         ]);
 
             }else{
-                $BalQty = $curr_quan;
+                $BalQty = $prev_quan;
 
                 DB::table('material.stockexp')
                     ->insert([
