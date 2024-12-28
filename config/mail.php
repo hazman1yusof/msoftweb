6ee6b9744f130f7a286597553d04d76f057766e4
hazman1yusof <hazman.yusof@gmail.com>
2024-12-28 11:31:07 +0800

    asd

--- config/mail.php
+++ config/mail.php
@@ -82,7 +82,13 @@ return [
     | used globally for all e-mails that are sent by your application.
     |
     */
-
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
+    ],
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
