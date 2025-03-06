73b83b3afec28409eb593f85ab85f496240b09c5
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2025-03-06 11:28:29 +0800

    asd

--- app/Http/Controllers/defaultController.php
+++ app/Http/Controllers/defaultController.php
@@ -1041,15 +1041,15 @@ abstract class defaultController extends Controller{
     }
 
     public function getAmount($money){
-        $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
-        $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
+        // $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
+        // $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
 
-        $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+        // $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
 
-        $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
-        $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+        // $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
+        // $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
 
-        return (float) str_replace(',', '.', $removedThousandSeparator);
+        return (float) str_replace(',', '', $money);
     }
 
 
