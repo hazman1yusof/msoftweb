0ecb9961ef03a1ce47d16d2ce1bc54181676241a
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2025-03-19 12:22:16 +0800

    asd

--- app/Exports/stockBalance_xlsExport.php
+++ app/Exports/stockBalance_xlsExport.php
@@ -117,7 +117,7 @@ class stockBalance_xlsExport implements FromView, WithEvents, WithColumnWidths
                                 $join = $join->on('p.itemcode', '=', 's.itemcode');
                                 $join = $join->on('p.uomcode', '=', 's.uomcode');
                                 $join = $join->where('p.compcode', '=', session('compcode'));
-                                $join = $join->on('p.groupcode', '=', 'STOCK');
+                                $join = $join->where('p.groupcode', '=', 'STOCK');
                                 $join = $join->on('p.unit', '=', 's.unit');
                             })
                         ->join('sysdb.department as d', function($join){
