04608220528a1a0a2ba8fd7dbe73c35b5c461730
hazman1yusof <hazman.yusof@gmail.com>
2025-04-23 23:35:40 +0800

    aasdasd\

---
+++ app/Exports/acctenq_dateExport.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace App\Exports;
+
+use DB;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\BeforeSheet;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
+use DateTime;
+use Carbon\Carbon;
+use stdClass;
+
+class acctenq_dateExport implements FromView, WithEvents, WithColumnWidths, WithColumnFormatting
+{
+    
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    
+    public function __construct($glaccount,$fromdate,$todate)
+    {   
+        $this->glaccount = $glaccount;
+        $this->fromdate = $fromdate;
+        $this->todate = $todate;
+        $this->comp = DB::table('sysdb.company')
+                    ->where('compcode','=',session('compcode'))
+                    ->first();
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'D' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+        ];
+    }
+    
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 15,
+            'B' => 20,
+            'C' => 80,
+            'D' => 20,
+            'E' => 25,
+            'F' => 25,
+            'G' => 25,
+            'H' => 30,
+        ];
+    }
+    
+    public function view(): View
+    {
+        $glaccount = $this->glaccount;
+        $fromdate = $this->fromdate;
+        $todate = $this->todate;
+        $compname = $this->comp->name;
+
+        $table = DB::table('finance.gltran as gl')
+                        ->select('gl.id','gl.source','gl.trantype','gl.auditno','gl.postdate','gl.description','gl.reference','gl.drcostcode','gl.crcostcode','gl.cracc','gl.dracc','gl.amount','glcr.description as acctname_cr','gldr.description as acctname_dr')
+                        ->where(function($table) use ($glaccount){
+                            $table->orwhere('gl.dracc','=', $glaccount);
+                            $table->orwhere('gl.cracc','=', $glaccount);
+                        })
+                        ->leftJoin('finance.glmasref as glcr', function($join){
+                            $join = $join->on('glcr.glaccno', '=', 'gl.cracc')
+                                            ->where('glcr.compcode','=',session('compcode'));
+                        })
+                        ->leftJoin('finance.glmasref as gldr', function($join){
+                            $join = $join->on('gldr.glaccno', '=', 'gl.dracc')
+                                            ->where('gldr.compcode','=',session('compcode'));
+                        })
+                        ->where('gl.postdate', '>=', $this->fromdate)
+                        ->where('gl.postdate', '<=', $this->todate)
+                        ->orderBy('gl.postdate', 'asc')
+                        ->get();
+
+        foreach ($table as $key => $value) {
+            if($value->dracc == $this->glaccount){
+                $value->acccode = $value->cracc;
+                $value->costcode = $value->crcostcode;
+                $value->cramount = 0;
+                $value->dramount = $value->amount;
+                $value->acctname = $value->acctname_cr;
+            }else{
+                $value->acccode = $value->dracc;
+                $value->costcode = $value->drcostcode;
+                $value->cramount = $value->amount;
+                $value->dramount = 0;
+                $value->acctname = $value->acctname_dr;
+            }
+
+            // switch ($value->source) {
+            //     case 'OE':
+            //         $data = $this->oe_data($value);
+            //         break;
+            //     case 'PB':
+            //         $data = $this->pb_data($value);
+            //         break;
+            //     case 'AP':
+            //         $data = $this->ap_data($value);
+            //         break;
+            //     case 'CM':
+            //         $data = $this->cm_data($value);
+            //         break;
+            //     default:
+            //         $data = $this->oth_data($value);
+            //         break;
+            // }
+
+            // if(!empty($data)){
+            //     $value->desc_ = $data->desc;
+            //     $value->reference = $data->refe;
+            // }else{
+            //     $value->desc_ = ' ';
+            // }
+        }
+
+        // dd($table);
+
+        return view('finance.GL.acctenq_date.acctenq_dateExcel', compact('table','glaccount','compname','fromdate','todate'));
+    }
+    
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event){
+                $event->sheet->getPageSetup()->setPaperSize(9); // A4
+                
+                $event->sheet->getHeaderFooter()->setOddHeader('&C'.$this->comp->name."\Bank Recon"."\n"
+                .'&L'
+                .'PRINTED BY : '.session('username')
+                ."\nPAGE : &P/&N"
+                .'&R'.'PRINTED DATE : '.Carbon::now("Asia/Kuala_Lumpur")->format('d-m-Y')
+                ."\n".'PRINTED TIME : '.Carbon::now("Asia/Kuala_Lumpur")->format('H:i'));
+                
+                $event->sheet->getPageMargins()->setTop(1);
+                
+                $event->sheet->getPageSetup()->setRowsToRepeatAtTop([1,1]);
+                $event->sheet->getStyle('A:H')->getAlignment()->setWrapText(true);
+                $event->sheet->getPageSetup()->setFitToWidth(1);
+                $event->sheet->getPageSetup()->setFitToHeight(0);
+            },
+        ];
+    }
+
+
+    public function oe_data($obj){
+        $billsum = DB::table('debtor.billsum as bs')
+                        ->select('bs.chggroup','ch.description')
+                        ->leftJoin('hisdb.chgmast as ch', function($join){
+                            $join = $join->on('ch.chgcode', '=', 'bs.chggroup')
+                                            ->where('ch.compcode','=',session('compcode'));
+                        })
+                        ->where('bs.compcode',session('compcode'))
+                        ->where('bs.auditno',$obj->auditno)
+                        ->first();
+
+        $responce = new stdClass();
+        $responce->desc = $billsum->description;
+        $responce->refe = 'INV-'.$obj->reference;
+
+        return $responce;
+    }
+
+    public function pb_data($obj){
+        $responce = new stdClass();
+
+        if($obj->trantype == 'IN'){
+
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','IN')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = str_pad($obj->auditno, 7, "0", STR_PAD_LEFT);
+            return $responce;
+
+        }else if($obj->trantype == 'DN'){
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','DN')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = 'DN-'.str_pad($obj->auditno, 7, "0", STR_PAD_LEFT);
+            return $responce;
+
+        }else if($obj->trantype == 'CN'){
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','CN')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = 'CN-'.str_pad($obj->auditno, 7, "0", STR_PAD_LEFT);
+            return $responce;
+
+        }else if($obj->trantype == 'RC'){
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name','dbh.recptno')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','RC')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = $dbacthdr->recptno;
+            return $responce;
+
+        }else if($obj->trantype == 'RD'){
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name','dbh.recptno')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','RD')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = $dbacthdr->recptno;
+            return $responce;
+
+        }else if($obj->trantype == 'RF'){
+            $dbacthdr = DB::table('debtor.dbacthdr as dbh')
+                            ->select('dbh.payercode','dbm.name','dbh.recptno')
+                            ->leftJoin('debtor.debtormast as dbm', function($join){
+                                $join = $join->on('dbm.debtorcode', '=', 'dbh.payercode')
+                                                ->where('dbm.compcode','=',session('compcode'));
+                            })
+                            ->where('dbh.compcode',session('compcode'))
+                            ->where('dbh.source','=','PB')
+                            ->where('dbh.trantype','=','RF')
+                            ->where('dbh.auditno','=',$obj->auditno)
+                            ->first();
+
+            $obj->description = $dbacthdr->payercode;
+            $responce->desc = $dbacthdr->name;
+            $responce->refe = $dbacthdr->recptno;
+            return $responce;
+
+        }
+    }
+    
+    public function ap_data($obj){
+        return 0;
+    }
+    
+    public function cm_data($obj){
+        return 0;
+    }
+    
+    public function oth_data($obj){
+        $responce = new stdClass();
+
+        $exp1 = explode('</br>', $obj->description);
+        $exp2 = explode(' ', $obj->reference);
+
+        $obj->description = $exp1[0];
+        $responce->desc = $exp1[1];
+        $responce->refe = $exp2[0].'-'.$exp2[1];
+
+        return $responce;
+    }
+    
+}
---
+++ resources/views/finance/GL/acctenq_date/acctenq_dateExcel.blade.php
@@ -0,0 +1,52 @@
+<table>
+    <tr>
+        <td></td>
+        <td></td>
+        <td style="text-align: center">{{$compname}}</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td style="text-align: center">GL TRANSACTION REPORT</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td style="text-align: center">GL Account : {{$glaccount}}</td>
+        <td></td>
+    </tr>
+    <tr></tr>
+    <tr>
+        <td style="font-weight: bold">Src</td>
+        <td style="font-weight: bold">Type</td>
+        <td style="font-weight: bold">Post Date</td>
+        <td style="font-weight: bold">Cost Code</td>
+        <td style="font-weight: bold">Double Entry Acct</td>
+        <td style="font-weight: bold">Reference</td>
+        <td style="font-weight: bold">Description</td>
+        <td style="font-weight: bold">Debit</td>
+        <td style="font-weight: bold">Credit</td>
+        <td style="font-weight: bold">Balance</td>
+        <td style="font-weight: bold">Document No</td>
+        <td style="font-weight: bold">Units</td>
+    </tr>
+    @foreach ($table as $obj)
+        <tr>
+            <td>{{$obj->source}}</td>
+            <td>{{$obj->trantype}}</td>
+            <td>{{$obj->postdate}}</td>
+            <td>{{$obj->costcode}}</td>
+            <td>{{$obj->acccode}}</td>
+            <td>{{$obj->reference}}</td>
+            <td>{{$obj->description}}</td>
+            <td>{{$obj->dramount}}</td>
+            <td>{{$obj->cramount}}</td>
+            <td>{{$obj->cramount}}</td>
+            <td>{{$obj->auditno}}</td>
+            <td>{{$obj->auditno}}</td>
+        </tr>
+    @endforeach
+    <tr></tr>
+</table>
