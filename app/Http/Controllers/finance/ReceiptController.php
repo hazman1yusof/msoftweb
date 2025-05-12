caf1b58386ff5ae015a5660c5f7b2287cd597c98
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2025-05-12 12:04:47 +0800

    asd

--- app/Http/Controllers/finance/ReceiptController.php
+++ app/Http/Controllers/finance/ReceiptController.php
@@ -78,7 +78,8 @@ class ReceiptController extends defaultController
                             ->where('compcode',session('compcode'))
                             ->where('tillcode',$till_obj->tillcode)
                             ->where('cashier',$till_obj->lastuser)
-                            ->where('opendate','=',$till_obj->upddate);
+                            ->where('opendate','=',$till_obj->upddate)
+                            ->whereNull('closedate');
 
                 $lastrcnumber = $this->defaultTill($till_obj->tillcode,'lastrcnumber');
 
