3cc97e51cb22fcc45b735d4cab4cc8cf68fca3d5
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2025-06-05 11:50:24 +0800

    asd

--- app/Http/Controllers/finance/SalesOrderController.php
+++ app/Http/Controllers/finance/SalesOrderController.php
@@ -252,6 +252,10 @@ class SalesOrderController extends defaultController
                 $table = $table->Where(function ($table) use ($request){
                         $table->Where('db.mrn','like',$request->searchVal[0]);
                 });
+            }else if($request->searchCol[0] == 'db_auditno'){
+                $table = $table->Where(function ($table) use ($request){
+                        $table->Where('db.auditno','like',$request->searchVal[0]);
+                });
             }else{
                 $table = $table->Where(function ($table) use ($request){
                         $table->Where($request->searchCol[0],'like',$request->searchVal[0]);
