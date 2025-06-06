2ae52c2b6f78169ada499de1bdd1b7cb499af181
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2025-06-06 15:28:12 +0800

    asd

--- app/Http/Controllers/util/TestController.php
+++ app/Http/Controllers/util/TestController.php
@@ -5429,7 +5429,7 @@ class TestController extends defaultController
                 DB::table('debtor.dballoc')
                         ->where('idno',$obj->idno)
                         ->update([
-                            'allocstats' => 'DEACTIVE'
+                            'allocsts' => 'DEACTIVE'
                         ]);
             }
 
