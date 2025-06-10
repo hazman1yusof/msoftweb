bb5b2beefbf7d910bca9c7ec8664e1d9f470c7e0
AmiraAlias <nuramiralias@gmail.com>
2025-06-10 12:17:06 +0800

    menu rehab

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -338,6 +338,7 @@ class HomeController extends Controller
         $menu.="<li><a style='padding-left:9px;' title='GP List' class='clickable' programid='gp_list' targetURL='pat_mast?epistycode=OP&curpat=false&PatClass=OTC' >GP List</a></li>";
         $menu.="<li><a style='padding-left:9px' title='Current Patient' class='clickable' programid='curr_pat' targeturl='pat_mast?epistycode=OP&curpat=true&PatClass=HIS'>Current Patient</a></li>";
         $menu.="<li><a style='padding-left:9px;' title='Case Note' class='clickable' programid='casenote' targetURL='ptcare_doctornote' >Case Note</a></li>";
+        $menu.="<li><a style='padding-left:9px;' title='Rehabilitation' class='clickable' programid='rehab' targetURL='rehab' >Rehabilitation</a></li>";
 
         return $menu;
     }
