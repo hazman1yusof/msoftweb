dc97aaca222a2ee277662cc78d742c0f48bdd34e
LilyAliyya <lilyaliyya@gmail.com>
2025-06-19 15:59:29 +0800

    physio spinalcord

--- resources/views/rehab/rehab.blade.php
+++ resources/views/rehab/rehab.blade.php
@@ -275,7 +275,7 @@
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/oswestryQuest.js?v=1.1') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/neuroAssessment.js?v=1.1') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/motorScale.js?v=1.1') }}"></script>
-    <script type="text/javascript" src="{{ asset('patientcare/js/rehab/spinalCord.js?v=1') }}"></script>
+    <script type="text/javascript" src="{{ asset('patientcare/js/rehab/spinalCord.js?v=1.1') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/occupTherapy/occupTherapy.js?v=1.1') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/occupTherapy/occupTherapy_cognitive.js?v=1') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/rehab/occupTherapy/occupTherapy_barthel.js?v=1') }}"></script>
