1522a54aa0f4a97e8d454340eca7f5b26e2fa8f0
LilyAliyya <lilyaliyya@gmail.com>
2025-07-07 16:54:09 +0800

    operteam ipesakit

--- app/Http/Controllers/hisdb/OperTeamController.php
+++ app/Http/Controllers/hisdb/OperTeamController.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\defaultController;
 
 class OperTeamController extends defaultController
 {
+    
     public function __construct()
     {
         $this->middleware('auth');
@@ -36,7 +37,6 @@ class OperTeamController extends defaultController
         DB::enableQueryLog();
         switch($request->action){
             case 'save_table_oper_team':
-            
                 switch($request->oper){
                     case 'add':
                         return $this->add($request);
@@ -61,53 +61,53 @@ class OperTeamController extends defaultController
         try {
             
             DB::table('nursing.otteam')
-                    ->insert([
-                        'compcode' => session('compcode'),
-                        'mrn' => $request->mrn_oper_team,
-                        'episno' => $request->episno_oper_team,
-                        'confirmedPt' => $request->confirmedPt,
-                        'opSite_mark' => $request->opSite_mark,
-                        'opSite_na' => $request->opSite_na,
-                        'machine_check' => $request->machine_check,
-                        'machine_na' => $request->machine_na,
-                        'monitor_on' => $request->monitor_on,
-                        'monitor_na' => $request->monitor_na,
-                        'allergy_remark' => $request->allergy_remark,
-                        'ptAllergy' => $request->ptAllergy,
-                        'difficultAirway' => $request->difficultAirway,
-                        'difficultAirway_na' => $request->difficultAirway_na,
-                        'gxmgsh' => $request->gxmgsh,
-                        'gxmgsh_na' => $request->gxmgsh_na,
-                        'ivAccess' => $request->ivAccess,
-                        'apparatus_check' => $request->apparatus_check,
-                        'otTable_check' => $request->otTable_check,
-                        'whiteboard' => $request->whiteboard,
-                        'introTeam' => $request->introTeam,
-                        'bsi_confirmedPt' => $request->bsi_confirmedPt,
-                        'antibioProphy' => $request->antibioProphy,
-                        'displayImg' => $request->displayImg,
-                        'briefSurgeon' => $request->briefSurgeon,
-                        'anaesthReview' => $request->anaesthReview,
-                        'scrubnurseReview' => $request->scrubnurseReview,
-                        'perfusionistReview' => $request->perfusionistReview,
-                        'surgeonStart' => $request->surgeonStart,
-                        'periodicUpdate' => $request->periodicUpdate,
-                        'shoutOut' => $request->shoutOut,
-                        'preclosure' => $request->preclosure,
-                        'finalProcedure' => $request->finalProcedure,
-                        'finalCount' => $request->finalCount,
-                        'specimenlabel' => $request->specimenlabel,
-                        'specimenlabel_na' => $request->specimenlabel_na,
-                        'issuesAddressed' => $request->issuesAddressed,
-                        'specialinstruction' => $request->specialinstruction,
-                        'relative_remark' => $request->relative_remark,
-                        'informRelative' => $request->informRelative,
-                        'adduser'  => session('username'),
-                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
-                        'lastuser'  => session('username'),
-                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
-                        'computerid' => session('computerid'),
-                    ]);
+                ->insert([
+                    'compcode' => session('compcode'),
+                    'mrn' => $request->mrn_oper_team,
+                    'episno' => $request->episno_oper_team,
+                    'confirmedPt' => $request->confirmedPt,
+                    'opSite_mark' => $request->opSite_mark,
+                    'opSite_na' => $request->opSite_na,
+                    'machine_check' => $request->machine_check,
+                    'machine_na' => $request->machine_na,
+                    'monitor_on' => $request->monitor_on,
+                    'monitor_na' => $request->monitor_na,
+                    'allergy_remark' => $request->allergy_remark,
+                    'ptAllergy' => $request->ptAllergy,
+                    'difficultAirway' => $request->difficultAirway,
+                    'difficultAirway_na' => $request->difficultAirway_na,
+                    'gxmgsh' => $request->gxmgsh,
+                    'gxmgsh_na' => $request->gxmgsh_na,
+                    'ivAccess' => $request->ivAccess,
+                    'apparatus_check' => $request->apparatus_check,
+                    'otTable_check' => $request->otTable_check,
+                    'whiteboard' => $request->whiteboard,
+                    'introTeam' => $request->introTeam,
+                    'bsi_confirmedPt' => $request->bsi_confirmedPt,
+                    'antibioProphy' => $request->antibioProphy,
+                    'displayImg' => $request->displayImg,
+                    'briefSurgeon' => $request->briefSurgeon,
+                    'anaesthReview' => $request->anaesthReview,
+                    'scrubnurseReview' => $request->scrubnurseReview,
+                    'perfusionistReview' => $request->perfusionistReview,
+                    'surgeonStart' => $request->surgeonStart,
+                    'periodicUpdate' => $request->periodicUpdate,
+                    'shoutOut' => $request->shoutOut,
+                    'preclosure' => $request->preclosure,
+                    'finalProcedure' => $request->finalProcedure,
+                    'finalCount' => $request->finalCount,
+                    'specimenlabel' => $request->specimenlabel,
+                    'specimenlabel_na' => $request->specimenlabel_na,
+                    'issuesAddressed' => $request->issuesAddressed,
+                    'specialinstruction' => $request->specialinstruction,
+                    'relative_remark' => $request->relative_remark,
+                    'informRelative' => $request->informRelative,
+                    'adduser'  => session('username'),
+                    'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    'lastuser'  => session('username'),
+                    'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    'computerid' => session('computerid'),
+                ]);
             
             DB::commit();
             
@@ -206,52 +206,69 @@ class OperTeamController extends defaultController
         try {
             
             DB::table('nursing.otteam')
-                    ->insert([
-                        'compcode' => session('compcode'),
-                        'mrn' => $request->mrn_oper_team,
-                        'episno' => $request->episno_oper_team,
-                        // BEFORE INDUCTION OF ANAESTHESIA
-                        'confirmedPt' => $request->confirmedPt,
-                        'opSite_mark' => $request->opSite_mark,
-                        'opSite_na' => $request->opSite_na,
-                        'machine_check' => $request->machine_check,
-                        'pulseoximeter' => $request->pulseoximeter,
-                        'ptAllergy' => $request->ptAllergy,
-                        'difficultAirway' => $request->difficultAirway,
-                        'bloodloss' => $request->bloodloss,
-                        'ivAccess' => $request->ivAccess,
-                        // BEFORE SKIN INCISION
-                        'whiteboard' => $request->whiteboard,
-                        'introTeam' => $request->introTeam,
-                        'bsi_confirmedPt' => $request->bsi_confirmedPt,
-                        'antibioProphy' => $request->antibioProphy,
-                        'antibioProphy_na' => $request->antibioProphy_na,
-                        'displayImg_na' => $request->displayImg_na,
-                        'displayImg' => $request->displayImg,
-                        'briefSurgeon' => $request->briefSurgeon,
-                        'surgeonReview_remark' => $request->surgeonReview_remark,
-                        'anaesthReview' => $request->anaesthReview,
-                        'scrubnurseReview' => $request->scrubnurseReview,
-                        // DURING PROCEDURE
-                        'procedure_hdr' => $request->procedure_hdr,
-                        'checkin' => $request->checkin,
-                        'periodicUpdate' => $request->periodicUpdate,
-                        'shoutOut' => $request->shoutOut,
-                        'preclosure' => $request->preclosure,
-                        // BEFORE PATIENT LEAVES OPERATING ROOM
-                        'finalProcedure' => $request->finalProcedure,
-                        'finalCount' => $request->finalCount,
-                        'specimenlabel' => $request->specimenlabel,
-                        'issuesAddressed' => $request->issuesAddressed,
-                        'specialinstruction' => $request->specialinstruction,
-                        'informRelative' => $request->informRelative,
-                        'coordinator' => $request->coordinator,
-                        'adduser'  => session('username'),
-                        'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
-                        'lastuser'  => session('username'),
-                        'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
-                        'computerid' => session('computerid'),
-                    ]);
+                ->insert([
+                    'compcode' => session('compcode'),
+                    'mrn' => $request->mrn_oper_team,
+                    'episno' => $request->episno_oper_team,
+                    'iPesakit' => $request->iPesakit,
+                    // BEFORE INDUCTION OF ANAESTHESIA
+                    'confirmedPt' => $request->confirmedPt,
+                    'opSite_mark' => $request->opSite_mark,
+                    'opSite_na' => $request->opSite_na,
+                    'machine_check' => $request->machine_check,
+                    'pulseoximeter' => $request->pulseoximeter,
+                    'ptAllergy' => $request->ptAllergy,
+                    'difficultAirway' => $request->difficultAirway,
+                    'bloodloss' => $request->bloodloss,
+                    'ivAccess' => $request->ivAccess,
+                    // BEFORE SKIN INCISION
+                    'whiteboard' => $request->whiteboard,
+                    'introTeam' => $request->introTeam,
+                    'bsi_confirmedPt' => $request->bsi_confirmedPt,
+                    'antibioProphy' => $request->antibioProphy,
+                    'antibioProphy_na' => $request->antibioProphy_na,
+                    'displayImg_na' => $request->displayImg_na,
+                    'displayImg' => $request->displayImg,
+                    'briefSurgeon' => $request->briefSurgeon,
+                    'surgeonReview_remark' => $request->surgeonReview_remark,
+                    'anaesthReview' => $request->anaesthReview,
+                    'scrubnurseReview' => $request->scrubnurseReview,
+                    // DURING PROCEDURE
+                    'procedure_hdr' => $request->procedure_hdr,
+                    'checkin' => $request->checkin,
+                    'periodicUpdate' => $request->periodicUpdate,
+                    'shoutOut' => $request->shoutOut,
+                    'preclosure' => $request->preclosure,
+                    // BEFORE PATIENT LEAVES OPERATING ROOM
+                    'finalProcedure' => $request->finalProcedure,
+                    'finalCount' => $request->finalCount,
+                    'specimenlabel' => $request->specimenlabel,
+                    'issuesAddressed' => $request->issuesAddressed,
+                    'specialinstruction' => $request->specialinstruction,
+                    'informRelative' => $request->informRelative,
+                    'coordinator' => $request->coordinator,
+                    'adduser'  => session('username'),
+                    'adddate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    'lastuser'  => session('username'),
+                    'lastupdate'  => Carbon::now("Asia/Kuala_Lumpur")->toDateString(),
+                    'computerid' => session('computerid'),
+                ]);
+            
+            if(!empty($request->iPesakit)){
+                $pat_mast = DB::table('hisdb.pat_mast')
+                            ->where('CompCode',session('compcode'))
+                            ->where('MRN',$request->mrn_oper_team)
+                            ->first();
+                
+                if($pat_mast->iPesakit != $request->iPesakit){
+                    DB::table('hisdb.pat_mast')
+                        ->where('CompCode',session('compcode'))
+                        ->where('MRN',$request->mrn_oper_team)
+                        ->update([
+                            'iPesakit' => $request->iPesakit,
+                        ]);
+                }
+            }
             
             DB::commit();
             
@@ -280,6 +297,7 @@ class OperTeamController extends defaultController
                 ->where('episno','=',$request->episno_oper_team)
                 ->where('compcode','=',session('compcode'))
                 ->update([
+                    'iPesakit' => $request->iPesakit,
                     // BEFORE INDUCTION OF ANAESTHESIA
                     'confirmedPt' => $request->confirmedPt,
                     'opSite_mark' => $request->opSite_mark,
@@ -323,6 +341,22 @@ class OperTeamController extends defaultController
                     'computerid' => session('computerid'),
                 ]);
             
+            if(!empty($request->iPesakit)){
+                $pat_mast = DB::table('hisdb.pat_mast')
+                            ->where('CompCode',session('compcode'))
+                            ->where('MRN',$request->mrn_oper_team)
+                            ->first();
+                
+                if($pat_mast->iPesakit != $request->iPesakit){
+                    DB::table('hisdb.pat_mast')
+                        ->where('CompCode',session('compcode'))
+                        ->where('MRN',$request->mrn_oper_team)
+                        ->update([
+                            'iPesakit' => $request->iPesakit,
+                        ]);
+                }
+            }
+            
             // $queries = DB::getQueryLog();
             // dump($queries);
             
@@ -348,7 +382,12 @@ class OperTeamController extends defaultController
                     ->where('compcode','=',session('compcode'))
                     ->where('mrn','=',$request->mrn)
                     ->where('episno','=',$request->episno);
-                    
+        
+        $patmast_obj = DB::table('hisdb.pat_mast')
+                        ->select('iPesakit')
+                        ->where('compcode',session('compcode'))
+                        ->where('mrn','=',$request->mrn);
+        
         $responce = new stdClass();
         
         if($otteam_obj->exists()){
@@ -356,8 +395,15 @@ class OperTeamController extends defaultController
             $responce->otteam = $otteam_obj;
         }
         
+        if($patmast_obj->exists()){
+            $patmast_obj = $patmast_obj->first();
+            
+            $iPesakit = $patmast_obj->iPesakit;
+            $responce->iPesakit = $iPesakit;
+        }
+        
         return json_encode($responce);
-    
+        
     }
     
 }
