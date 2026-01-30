b3fe24725761c59a6ffc31b20c6f20d7ddcda559
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2026-01-30 14:53:49 +0800

    asd

--- resources/views/finance/AP/APAgeingDtl_Report/APAgeingDtl_Report_excel_stmt.blade.php
+++ resources/views/finance/AP/APAgeingDtl_Report/APAgeingDtl_Report_excel_stmt.blade.php
@@ -82,61 +82,61 @@
             @endif
         @endforeach
 
-        @foreach($ap_pv_main as $pv_main)
-            @if($pv_main->ap1_suppcode == $supp->suppcode)
-                @php($totalAmount -= $pv_main->ap1_amount)
-                <tr>
-                    <td>{{\Carbon\Carbon::parse($pv_main->ap1_postdate)->format('d/m/Y')}}</td>
-                    <td data-format="@">{{str_pad($pv_main->ap1_auditno, 7, "0", STR_PAD_LEFT)}}</td>
+    @endforeach
 
-                    <td data-format="@">{{$pv_main->ap1_trantype}}-{{$pv_main->ap1_pvno}}</td>
-                    <td data-format="@">{{$pv_main->ap1_remarks}}</td>
-                    <td style="text-align: right">-{{$pv_main->ap1_amount}}</td>
+    @foreach($ap_pv_main as $pv_main)
+        @php($totalAmount -= $pv_main->ap1_amount)
+        <tr>
+            <td>{{\Carbon\Carbon::parse($pv_main->ap1_postdate)->format('d/m/Y')}}</td>
+            <td data-format="@">{{str_pad($pv_main->ap1_auditno, 7, "0", STR_PAD_LEFT)}}</td>
+
+            <td data-format="@">{{$pv_main->ap1_trantype}}-{{$pv_main->ap1_pvno}}</td>
+            <td data-format="@">{{$pv_main->ap1_remarks}}</td>
+            <td style="text-align: right">-{{$pv_main->ap1_amount}}</td>
+            <td style="text-align: right">{{$totalAmount}}</td>
+        </tr>
+        @foreach($ap_pv as $pv_obj)
+            @if($pv_obj->ap1_auditno == $pv_main->ap1_auditno)
+                @php($totalAmount += $pv_obj->ap2_amount)
+                <tr>
+                    <td>{{\Carbon\Carbon::parse($pv_obj->ap2_postdate)->format('d/m/Y')}}</td>
+                    <td data-format="@">{{str_pad($pv_obj->ap2_auditno, 7, "0", STR_PAD_LEFT)}}</td>
+
+                    <td data-format="@">{{$pv_obj->ap2_document}}</td>
+                    <td data-format="@">{{$pv_obj->ap2_remarks}}</td>
+                    <td style="text-align: right">{{$pv_obj->ap2_amount * -1}}</td>
                     <td style="text-align: right">{{$totalAmount}}</td>
                 </tr>
-                @foreach($ap_pv as $pv_obj)
-                    @if($pv_obj->ap1_auditno == $pv_main->ap1_auditno)
-                        @php($totalAmount += $pv_obj->ap2_amount)
-                        <tr>
-                            <td>{{\Carbon\Carbon::parse($pv_obj->ap2_postdate)->format('d/m/Y')}}</td>
-                            <td data-format="@">{{str_pad($pv_obj->ap2_auditno, 7, "0", STR_PAD_LEFT)}}</td>
-
-                            <td data-format="@">{{$pv_obj->ap2_document}}</td>
-                            <td data-format="@">{{$pv_obj->ap2_remarks}}</td>
-                            <td style="text-align: right">{{$pv_obj->ap2_amount * -1}}</td>
-                            <td style="text-align: right">{{$totalAmount}}</td>
-                        </tr>
-                    @endif
-                @endforeach
             @endif
         @endforeach
-        
-        <tr></tr>
-        <tr>
-            <td></td>
-            @foreach ($grouping as $key => $group)
-                @if($key+1 < count($grouping))
-                <td style="font-weight:bold; text-align: left">{{$group+1}}-{{$grouping[$key+1]}} Days</td>
-                @else
-                <td style="font-weight:bold; text-align: left">> {{$group}} Days</td>
-                @endif
-            @endforeach
-            <td style="font-weight:bold; text-align: left">Total</td>
-        </tr>
-
-        <tr>
-            <td></td>
-            @php($total_line = 0)
-        @foreach ($grouping_tot as $key => $val)
-            @php($total_line = $total_line + $val)
-            <td>{{$val}}</td>
-        @endforeach
-            <td>{{$total_line}}</td>
-        </tr>
-
-        <tr></tr>
-        <div style="page-break-after: always" />
     @endforeach
+    
+    <tr></tr>
+    <tr>
+        <td></td>
+        @foreach ($grouping as $key => $group)
+            @if($key+1 < count($grouping))
+            <td style="font-weight:bold; text-align: left">{{$group+1}}-{{$grouping[$key+1]}} Days</td>
+            @else
+            <td style="font-weight:bold; text-align: left">> {{$group}} Days</td>
+            @endif
+        @endforeach
+        <td style="font-weight:bold; text-align: left">Total</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        @php($total_line = 0)
+    @foreach ($grouping_tot as $key => $val)
+        @php($total_line = $total_line + $val)
+        <td>{{$val}}</td>
+    @endforeach
+        <td>{{$total_line}}</td>
+    </tr>
+
+    <tr></tr>
+    <div style="page-break-after: always" />
+
     <tr></tr>
     <tr>
         <td></td>
