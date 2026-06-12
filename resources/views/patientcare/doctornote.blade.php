95d9d692a0b4b840675b27538cb2722aaa640926
AmiraAlias <nuramiralias@gmail.com>
2026-06-12 15:52:42 +0800

    phy doctornote

--- resources/views/patientcare/doctornote.blade.php
+++ resources/views/patientcare/doctornote.blade.php
@@ -501,8 +501,8 @@
     <script type="text/javascript" src="{{ asset('patientcare/js/doctornote.js?v=1.6') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/nursing.js?v=1.3') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/dieteticCareNotes.js?v=1.2') }}"></script>
-    <script type="text/javascript" src="{{ asset('patientcare/js/physioterapy.js?v=1.1') }}"></script>
-    <script type="text/javascript" src="{{ asset('patientcare/js/physioterapy_ncase.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('patientcare/js/physiotherapy/physioterapy.js?v=1.1') }}"></script>
+    <script type="text/javascript" src="{{ asset('patientcare/js/physiotherapy/physioterapy_ncase.js') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/doctornote_main.js?v=1.2') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/requestfor.js?v=1.9') }}"></script>
     <script type="text/javascript" src="{{ asset('patientcare/js/admhandover.js?v=1.1') }}"></script>
