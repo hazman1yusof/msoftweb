8db09983ddfd4e338cb19fb696be1477513bb822
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2026-07-06 11:23:04 +0800

    asd

--- public/js/hisdb/pat_mgmt/biodata.js
+++ public/js/hisdb/pat_mgmt/biodata.js
@@ -224,8 +224,10 @@ function save_patient(oper,idno,mrn="nothing"){
     // var image = ($("img#photobase64").attr('src').startsWith('data'))?
     //             {PatientImage:$("img#photobase64").attr('src')}:
     //             {PatientImage:null}
+                
+    var PatientImage_ = ($("img#photobase64").attr('src').startsWith('data'))?$("img#photobase64").attr('src'):null;
 
-    var image = {PatientImage:null,field:['Name','MRN','Newic','Oldic','ID_Type','idnumber','DOB','telh','telhp','Email','AreaCode','Sex','Citizencode','RaceCode','TitleCode','Religion','MaritalCode','LanguageCode','Remarks','RelateCode','CorpComp','Staffid','OccupCode','Email_official','Childno','Address1','Address2','Address3','Offadd1','Offadd2','Offadd3','pAdd1','pAdd2','pAdd3','Postcode','OffPostcode','pPostCode','Active','Confidential','MRFolder','PatientCat','NewMrn','bloodgrp','Episno','first_visit_date','last_visit_date','loginid','pat_category','MRFolder','bloodgrp','NewMrn','iPesakit'],};
+    var image = {PatientImage:PatientImage_,field:['Name','MRN','Newic','Oldic','ID_Type','idnumber','DOB','telh','telhp','Email','AreaCode','Sex','Citizencode','RaceCode','TitleCode','Religion','MaritalCode','LanguageCode','Remarks','RelateCode','CorpComp','Staffid','OccupCode','Email_official','Childno','Address1','Address2','Address3','Offadd1','Offadd2','Offadd3','pAdd1','pAdd2','pAdd3','Postcode','OffPostcode','pPostCode','Active','Confidential','MRFolder','PatientCat','NewMrn','bloodgrp','Episno','first_visit_date','last_visit_date','loginid','pat_category','MRFolder','bloodgrp','NewMrn','iPesakit'],};
 
     $.post( "./pat_mast/save_patient?"+$.param(saveParam), $("#frm_patient_info").serialize()+'&'+$.param(postobj)+'&'+$.param(image) , function( data ) {
         
