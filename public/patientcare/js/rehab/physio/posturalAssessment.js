9e2989d2eec327bbe28b39fd8b94aa8ce27b2bee
LilyAliyya <lilyaliyya@gmail.com>
2026-08-18 11:03:51 +0800

    rehab physio view drawing

--- public/patientcare/js/rehab/physio/posturalAssessment.js
+++ public/patientcare/js/rehab/physio/posturalAssessment.js
@@ -141,9 +141,11 @@ $(document).ready(function (){
         
         if(mrn.trim() == '' || type.trim() == ''){
             alert('Please choose Patient First');
-        }else if($('#save_posturalAssessment').prop('disabled')){
-            alert('Edit this patient first');
-        }else if(entereddate == ''){
+        }
+        // else if($('#save_posturalAssessment').prop('disabled')){
+        //     alert('Edit this patient first');
+        // }
+        else if(entereddate == ''){
             alert('Please enter date first');
         }else{
             if(istablet){
