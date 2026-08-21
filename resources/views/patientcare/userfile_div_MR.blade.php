4e45ebe4e3230837319b26f2a06cc6d9dbe3f531
AmiraAlias <nuramiralias@gmail.com>
2026-08-21 15:54:20 +0800

    docimg_MR

---
+++ resources/views/patientcare/userfile_div_MR.blade.php
@@ -0,0 +1,39 @@
+<!-- <div class="ui teal segment" style="padding: 10px 10px 30px 30px;">
+	<h4 class="ui header">This user uploaded Files </h4>
+	<button class="tiny ui icon button" type="button" id="refresh_userfile_btn" style="
+		position: absolute;
+	    top: 5px;
+	    right: 20px;
+	    z-index: 5;
+	 ">
+	  <i class="redo icon"></i>
+	</button>
+	<button class="tiny ui orange icon button" type="button" id="upload_userfile_btn" style="
+		position: absolute;
+	    top: 5px;
+	    right: 60px;
+	    z-index: 5;
+	 "> Upload
+	  <i class="cloud upload icon"></i>
+	</button>
+	<input type="file" id="upload_userfile_fld" name="upload_userfile_fld" style="display: none;">
+	<table class="ui celled table" id='tablePreview'>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Date</th>
+				<th>Remark</th>
+				<th>File Preview</th>
+				<th>MRN</th>
+				<th>Add User</th>
+				<th>Add Date</th>
+				<th>Download</th>
+				<th>type</th>
+			</tr>
+		</thead>
+	</table>
+</div> -->
+
+<div id=""  style="">
+    <iframe style="display:block; border:none; height:95vh; width:95vw;" id="userfile_iframe_MR"></iframe>
+</div>
